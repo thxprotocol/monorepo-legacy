bf4f3613d94d57acc25b2774e537e28799dca0c7
Peter <peter@thx.network>
2022-10-08 10:53:21 +0200

    Fixes redirect url

--- apps/dashboard/src/utils/secrets.ts
+++ apps/dashboard/src/utils/secrets.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.VUE_APP_BASE_URL || '';
+export const BASE_URL = process.env.VUE_APP_DASHBOARD_URL || '';
 export const AUTH_URL = process.env.VUE_APP_AUTH_URL || '';
 export const API_URL = process.env.VUE_APP_API_ROOT || '';
 export const PUBLIC_URL = process.env.VUE_APP_PUBLIC_URL || '';
