d470164ecfd511d8b614d64ed0b46e9e8bb78da3
Peter <peter@thx.network>
2022-10-09 17:06:41 +0200

    Trigger api build

--- apps/api/src/app/jobs/updatePendingTransactions.ts
+++ apps/api/src/app/jobs/updatePendingTransactions.ts
@@ -1,15 +1,20 @@
 import { TransactionState, TransactionType } from '@thxnetwork/api/types/enums';
-import { Transaction, TransactionDocument } from '@thxnetwork/api/models/Transaction';
+import {
+  Transaction,
+  TransactionDocument,
+} from '@thxnetwork/api/models/Transaction';
 import { logger } from '@thxnetwork/api/util/logger';
 import TransactionService from '@thxnetwork/api/services/TransactionService';
 
 export async function updatePendingTransactions() {
-    const transactions: TransactionDocument[] = await Transaction.find({
-        state: TransactionState.Sent,
-        type: TransactionType.Relayed,
-    }).sort({ createdAt: 'asc' });
+  const transactions: TransactionDocument[] = await Transaction.find({
+    state: TransactionState.Sent,
+    type: TransactionType.Relayed,
+  }).sort({ createdAt: 'asc' });
 
-    for (const tx of transactions) {
-        TransactionService.queryTransactionStatusDefender(tx).catch((error) => logger.error(error));
-    }
+  for (const tx of transactions) {
+    TransactionService.queryTransactionStatusDefender(tx).catch((error) =>
+      logger.error(error)
+    );
+  }
 }
