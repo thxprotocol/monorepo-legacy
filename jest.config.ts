753d0bcf91f4760482ae695846339b96ef59bc42
Peter <peter@thx.network>
2022-10-10 22:24:33 +0200

    Exports package json for auth

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,4 @@ import { getJestProjects } from '@nrwl/jest';
 
 export default {
     projects: getJestProjects(),
-    transform: {
-        '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
