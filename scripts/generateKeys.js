fb991beb3d9d1e7722f96b1b18b7926d93d195c2
Bram Rongen <bram@thx.network>
2022-10-11 14:57:29 +0200

    Remove unnecessary files

--- scripts/generateKeys.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const jose = require('jose');
-const keystore = new jose.JWKS.KeyStore();
-
-Promise.all([keystore.generate('RSA', 2048, { use: 'sig', alg: 'RS256' })]).then(() => {
-    fs.writeFileSync(path.resolve('src/jwks.json'), JSON.stringify(keystore.toJWKS(true), null, 2));
-});
