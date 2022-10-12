f97ee3178a8448f22b912dececdf584f3c8d2c11
Peter <peter@thx.network>
2022-10-12 21:02:51 +0200

    Prettify some and fix image url in CardNFT

--- apps/wallet/src/types/Payments.ts
+++ apps/wallet/src/types/Payments.ts
@@ -4,37 +4,37 @@ import { TPromotion } from '@thxnetwork/wallet/store/modules/promotions';
 import { ChainId } from './enums/ChainId';
 
 export enum PaymentState {
-  Requested = 0,
-  Pending = 1,
-  Completed = 2,
-  Failed = 3,
+    Requested = 0,
+    Pending = 1,
+    Completed = 2,
+    Failed = 3,
 }
 
 export interface IPayments {
-  [id: string]: TPayment;
+    [id: string]: TPayment;
 }
 
 export type TPayment = {
-  _id: string;
-  id: string;
-  amount: string;
-  tokenSymbol: string;
-  tokenAddress: string;
-  poolId: string;
-  chainId: ChainId;
-  sender: string;
-  receiver: string;
-  transactions: string[];
-  state: PaymentState;
-  token: string;
-  paymentUrl: string;
-  successUrl: string;
-  failUrl: string;
-  cancelUrl: string;
-  createdAt: Date;
-  updatedAt: Date;
-  metadataId?: string;
-  metadata?: TERC721Metadata;
-  promotion?: TPromotion;
-  membership?: TMembership;
+    _id: string;
+    id: string;
+    amount: string;
+    tokenSymbol: string;
+    tokenAddress: string;
+    poolId: string;
+    chainId: ChainId;
+    sender: string;
+    receiver: string;
+    transactions: string[];
+    state: PaymentState;
+    token: string;
+    paymentUrl: string;
+    successUrl: string;
+    failUrl: string;
+    cancelUrl: string;
+    createdAt: Date;
+    updatedAt: Date;
+    metadataId?: string;
+    metadata?: TERC721Metadata;
+    promotion?: TPromotion;
+    membership?: TMembership;
 };
