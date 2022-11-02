7b2422862b78633fb2f784885e32d6e0967baa82
GarfDev <garfdev.13@gmail.com>
2022-11-02 15:22:41 +0700

    fix: remove vue 3 dependency, using vue demi

--- apps/widget/src/router/index.ts
+++ apps/widget/src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router4';
 import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
-        component: Home,
+        component: Home as any,
     },
     {
         path: '/about',
--- apps/widget/src/views/Home.vue
+++ apps/widget/src/views/Home.vue
@@ -5,7 +5,7 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '../components/HelloWorld.vue';
 
