6c8e05b6e2858b8d85e87339fbf854ed51ee25b5
Peter <peter@thx.network>
2022-11-09 16:04:40 +0100

    Formatting and scope request fix for wallet

--- apps/wallet/src/utils/oidc.ts
+++ apps/wallet/src/utils/oidc.ts
@@ -8,7 +8,7 @@ const thxClient = new THXClient({
     redirectUrl: `${BASE_URL}/signin-oidc`,
     clientId: OIDC_CLIENT_ID,
     clientSecret: OIDC_CLIENT_SECRET,
-    scopes: 'openid rewards:read erc20:read erc721:read withdrawals:read withdrawals:write deposits:read deposits:write account:read account:write memberships:read memberships:write promotions:read transactions:read relay:write swaprule:read swap:read swap:write wallets:read wallets:write',
+    scopes: 'openid rewards:read erc20:read erc721:read withdrawals:read withdrawals:write deposits:read deposits:write account:read account:write memberships:read memberships:write promotions:read transactions:read relay:write swaprule:read swap:read swap:write claims:read wallets:read wallets:write',
     automaticSilentRenew: true,
     silent_redirect_uri: `${BASE_URL}/silent-renew.html`,
 });
