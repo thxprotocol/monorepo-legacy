c2161ff4c32947b50cd60ea381983dca1d8db33a
Peter <peter@thx.network>
2022-11-10 20:31:04 +0100

    Adjusts injection scripts

--- apps/wallet/public/assets/scripts/inject.js
+++ apps/wallet/public/assets/scripts/inject.js
@@ -1,47 +1,52 @@
 async function createBaseElements() {
-  const main = document.createElement("div");
-  const iframe = document.createElement("iframe");
-  const trigger = document.createElement("img");
+    const main = document.createElement('div');
+    const iframe = document.createElement('iframe');
+    const trigger = document.createElement('img');
 
-  // Setup class name
-  main.className = "thx-container";
-  main.id = "thx-container";
-  iframe.className = "thx-iframe";
-  iframe.id = "thx-iframe";
-  trigger.className = "thx-trigger";
-  trigger.id = "thx-trigger";
+    // Setup class name
+    main.id = 'thx-container';
 
-  iframe.src = "https://wallet.thx.network";
-  trigger.src = "https://img.upanh.tv/2022/10/31/logo.png";
+    iframe.id = 'thx-iframe';
+    iframe.src = 'https://localhost:8080';
+    iframe.style.display = 'none';
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '100px';
+    iframe.style.right = '15px';
+    iframe.style.height = '680px';
+    iframe.style.width = '400px';
+    iframe.style.border = '0';
+    iframe.style.borderRadius = '10px';
 
-  trigger.addEventListener("click", toggleTrigger);
+    trigger.id = 'thx-trigger';
+    trigger.src = 'https://img.upanh.tv/2022/10/31/logo.png';
+    trigger.style.width = '52px';
+    trigger.style.height = '52px';
+    trigger.style.opacity = 0.5;
+    trigger.style.transition = 'all 300ms';
+    trigger.style.cursor = 'pointer';
+    trigger.style.position = 'fixed';
+    trigger.style.bottom = '15px';
+    trigger.style.right = '15px';
+    trigger.addEventListener('click', toggleTrigger);
 
-  main.appendChild(iframe);
-  main.appendChild(trigger);
+    main.appendChild(iframe);
+    main.appendChild(trigger);
 
-  document.body.appendChild(main);
-
-  return [main, iframe, trigger];
+    document.body.appendChild(main);
 }
 
-async function toggleTrigger(e) {
-  const iframe = document.getElementById("thx-iframe");
-
-  const cursorX = e.pageX;
-  const cursorY = e.pageY;
-
-  const isVisible = iframe.classList.contains("active");
-
-  iframe.style.left = `calc(${cursorX}px - 320px)`;
-  iframe.style.bottom = `calc(${cursorY}px - 440px)`;
-
-  if (isVisible)
-    document.getElementById("thx-iframe").classList.remove("active");
-  else document.getElementById("thx-iframe").classList.add("active");
+function toggleTrigger() {
+    const iframe = document.getElementById('thx-iframe');
+    iframe.style.display = iframe.style.display === 'block' ? 'none' : 'block';
 }
 
-async function initialize() {
-  const [mainDiv, iframe, trigger] = await createBaseElements();
+function initialize() {
+    createBaseElements();
+
+    window.onmessage = (event) => {
+        if (event.origin !== 'https://localhost:8080') return;
+        toggleTrigger();
+    };
 }
 
 initialize();
