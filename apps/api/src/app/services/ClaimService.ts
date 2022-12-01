a0030e55af1c7dfbd10eb52ddc8c00ae524c6f09
Peter <peter@thx.network>
2022-12-01 09:19:45 +0100

    CLaim UUID fixes

--- apps/api/src/app/services/ClaimService.ts
+++ apps/api/src/app/services/ClaimService.ts
@@ -2,34 +2,20 @@ import { AssetPoolDocument } from '@thxnetwork/api/models/AssetPool';
 import { Claim } from '@thxnetwork/api/models/Claim';
 import { TClaim } from '@thxnetwork/api/types/TClaim';
 import { TBaseReward } from '@thxnetwork/types/';
-import AssetPoolService from './AssetPoolService';
 import db from '@thxnetwork/api/util/database';
 
-export default {
-    create: (data: { poolId: string; rewardId: string; erc20Id?: string; erc721Id?: string }) => {
-        const claim = { id: db.createUUID(), ...data } as TClaim;
-        return Claim.create(claim);
-    },
-    findById: (id: string) => {
-        return Claim.findOne({ id });
-    },
-    findByPool: (pool: AssetPoolDocument) => {
-        return Claim.find({ poolId: String(pool._id) });
-    },
-    findByReward: (reward: TBaseReward) => {
-        return Claim.find({ rewardId: reward._id, poolId: reward.poolId });
-    },
-    findByHash: async (hash: string) => {
-        const rewardData = JSON.parse(Buffer.from(hash, 'base64').toString());
+function create(data: { poolId: string; rewardId: string; erc20Id?: string; erc721Id?: string }) {
+    const claim = { id: db.createUUID(), ...data } as TClaim;
+    return Claim.create(claim);
+}
+function findById(id: string) {
+    return Claim.findOne({ id });
+}
+function findByPool(pool: AssetPoolDocument) {
+    return Claim.find({ poolId: String(pool._id) });
+}
+function findByReward(reward: TBaseReward) {
+    return Claim.find({ rewardId: reward.uuid, poolId: reward.poolId });
+}
 
-        // With this statement we support hashes that dont contain a poolId
-        if (!rewardData.poolId && rewardData.poolAddress) {
-            const pool = await AssetPoolService.getByAddress(rewardData.poolAddress);
-            if (!pool) return;
-            rewardData.poolId = String(pool._id);
-        }
-
-        const { rewardId, poolId } = rewardData;
-        return Claim.findOne({ rewardId, poolId });
-    },
-};
+export default { create, findById, findByPool, findByReward };
