28fcf87b08d9ba9afa456d5668e150eafc4edbdd
Peter <peter@thx.network>
2022-12-11 10:46:43 +0100

    Adds metadata to perk resource

--- apps/api/src/app/controllers/erc721-perks/get.controller.ts
+++ apps/api/src/app/controllers/erc721-perks/get.controller.ts
@@ -3,13 +3,13 @@ import { NotFoundError } from '@thxnetwork/api/util/errors';
 import { param } from 'express-validator';
 import WithdrawalService from '@thxnetwork/api/services/WithdrawalService';
 import ClaimService from '@thxnetwork/api/services/ClaimService';
-import RewardNftService from '@thxnetwork/api/services/ERC721PerkService';
+import ERC721PerkService from '@thxnetwork/api/services/ERC721PerkService';
 
 const validation = [param('id').exists()];
 
 const controller = async (req: Request, res: Response) => {
     // #swagger.tags = ['RewardsNft']
-    const reward = await RewardNftService.get(req.params.id);
+    const reward = await ERC721PerkService.get(req.params.id);
     if (!reward) throw new NotFoundError();
 
     const claims = await ClaimService.findByReward(reward);
--- apps/api/src/app/controllers/perks/list.controller.ts
+++ apps/api/src/app/controllers/perks/list.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { ERC20Perk } from '@thxnetwork/api/models/ERC20Perk';
 import { ERC721Perk } from '@thxnetwork/api/models/ERC721Perk';
+import ERC721Service from '@thxnetwork/api/services/ERC721Service';
 
 const controller = async (req: Request, res: Response) => {
     // #swagger.tags = ['Rewards']
@@ -17,19 +18,24 @@ const controller = async (req: Request, res: Response) => {
                 amount: r.amount,
                 pointPrice: r.pointPrice,
                 image: r.image,
+                isOwned: false,
             };
         }),
-        erc721Perks: erc721Perks.map((r) => {
-            return {
-                _id: r._id,
-                uuid: r.uuid,
-                title: r.title,
-                description: r.description,
-                erc721metadataId: r.erc721metadataId,
-                pointPrice: r.pointPrice,
-                image: r.image,
-            };
-        }),
+        erc721Perks: await Promise.all(
+            erc721Perks.map(async (r) => {
+                return {
+                    _id: r._id,
+                    uuid: r.uuid,
+                    title: r.title,
+                    description: r.description,
+                    erc721metadataId: r.erc721metadataId,
+                    metadata: await ERC721Service.findMetadataById(r.erc721metadataId),
+                    pointPrice: r.pointPrice,
+                    image: r.image,
+                    isOwned: false,
+                };
+            }),
+        ),
     });
 };
 
