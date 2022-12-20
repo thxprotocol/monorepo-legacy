1430777df818140220d89028d06c4de180c79b94
Garfdev <garfdev.13@gmail.com>
2022-12-20 13:54:25 +0700

    fix: make sure edit flow still stable

--- apps/dashboard/src/components/cards/BaseCardRewardCondition.vue
+++ apps/dashboard/src/components/cards/BaseCardRewardCondition.vue
@@ -116,18 +116,34 @@ export default class BaseCardRewardCondition extends Vue {
         switch (platform.type) {
             case RewardConditionPlatform.Google: {
                 await this.$store.dispatch('account/getYoutube');
-                this.onSelectInteraction(platformInteractionList[1]);
                 this.isAuthorized = !!this.youtube;
                 break;
             }
             case RewardConditionPlatform.Twitter: {
                 await this.$store.dispatch('account/getTwitter');
-                this.onSelectInteraction(platformInteractionList[3]);
                 this.isAuthorized = !!this.twitter;
                 break;
             }
             default:
         }
+
+        this.onSelectInteraction(this.interaction);
+
+        // Only initialize if there no `rewardCondition` prop inputed
+        if (!this.rewardCondition) {
+            switch (platform.type) {
+                case RewardConditionPlatform.Google: {
+                    this.onSelectInteraction(platformInteractionList[1]);
+                    break;
+                }
+                case RewardConditionPlatform.Twitter: {
+                    this.onSelectInteraction(platformInteractionList[3]);
+                    break;
+                }
+                default:
+            }
+        }
+
         this.isLoadingPlatform = false;
     }
 
