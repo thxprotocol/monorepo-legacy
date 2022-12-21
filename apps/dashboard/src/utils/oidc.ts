384faa911ded0d3aa0597b22c1df4845ee9908e1
Peter <peter@thx.network>
2022-12-21 21:19:35 +0100

    Fix silent renew for dashboard

--- apps/dashboard/src/utils/oidc.ts
+++ apps/dashboard/src/utils/oidc.ts
@@ -17,3 +17,5 @@ export const config: UserManagerSettings = {
     resource: API_URL,
     scope: 'openid account:read account:write pools:read pools:write erc20:read erc20:write erc721:read erc721:write rewards:read rewards:write withdrawals:read deposits:read deposits:write promotions:read promotions:write widgets:write widgets:read transactions:read members:read members:write payments:read payments:write swaprule:read swaprule:write claims:read brands:read brands:write clients:read clients:write erc20_rewards:read erc20_rewards:write erc721_rewards:read erc721_rewards:write referral_rewards:read referral_rewards:write',
 };
+
+localStorage.setItem('thx:dashboard:oidc', JSON.stringify(config));
