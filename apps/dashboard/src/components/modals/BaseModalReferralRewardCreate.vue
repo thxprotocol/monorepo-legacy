f108904ca02835a4e3ab870be35ea7bc81204655
Peter <peter@thx.network>
2023-01-02 13:29:13 +0100

    Improve referral URL modals

--- apps/dashboard/src/components/modals/BaseModalReferralRewardCreate.vue
+++ apps/dashboard/src/components/modals/BaseModalReferralRewardCreate.vue
@@ -2,11 +2,12 @@
     <base-modal size="xl" title="Create Referral Reward" :id="id" :error="error" :loading="isLoading">
         <template #modal-body v-if="!isLoading">
             <p class="text-gray">
-                Points rewards are distributed to your customers achieving milestones in your customer journey.
+                Referral rewards incentive your existing users to attract new users and will drive down your customer
+                acquisition costs.
             </p>
             <form v-on:submit.prevent="onSubmit" id="formRewardPointsCreate">
                 <b-row>
-                    <b-col md="12">
+                    <b-col md="6">
                         <b-form-group label="Title">
                             <b-form-input v-model="title" />
                         </b-form-group>
@@ -16,9 +17,40 @@
                         <b-form-group label="Amount">
                             <b-form-input v-model="amount" />
                         </b-form-group>
-                        <b-form-group label="Succes URL">
-                            <b-form-input v-model="successUrl" />
-                        </b-form-group>
+                    </b-col>
+                    <b-col md="6">
+                        <b-card body-class="bg-light p-0">
+                            <b-button
+                                class="d-flex align-items-center justify-content-between w-100"
+                                variant="light"
+                                v-b-toggle.collapse-card-expiry
+                            >
+                                <strong>Referral Completion</strong>
+                                <i :class="`fa-chevron-${isVisible ? 'up' : 'down'}`" class="fas m-0"></i>
+                            </b-button>
+                            <b-collapse id="collapse-card-expiry" v-model="isVisible">
+                                <hr class="mt-0" />
+                                <div class="px-3">
+                                    <b-form-group label="Success URL">
+                                        <b-form-input v-model="successUrl" />
+                                        <p class="small text-muted mt-2 mb-0">
+                                            When the user receiving the referral URL comes across this URL the referral
+                                            will be marked as successful.
+                                            <strong>E.g. https://example.com/thanks-for-your-signup</strong>
+                                        </p>
+                                    </b-form-group>
+                                    <b-alert show variant="warning">
+                                        <i class="fas fa-exclamation-circle mr-2"></i>
+                                        <strong class="mr-2">Important:</strong>Make sure the loyalty widget is running
+                                        on your success page.
+                                    </b-alert>
+                                    <hr />
+                                    <p class="text-muted font-italic">
+                                        Backend integration for completing referral rewards will be available soon.
+                                    </p>
+                                </div>
+                            </b-collapse>
+                        </b-card>
                     </b-col>
                 </b-row>
             </form>
@@ -54,6 +86,7 @@ import BaseCardRewardExpiry from '../cards/BaseCardRewardExpiry.vue';
 export default class ModalReferralRewardCreate extends Vue {
     isSubmitDisabled = false;
     isLoading = false;
+    isVisible = true;
     error = '';
     title = '';
     amount = '0';
