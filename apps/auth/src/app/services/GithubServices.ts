1b587e20d65e68042441788f049b0669dd834371
Garfdev <garfdev.13@gmail.com>
2023-01-03 20:25:45 +0700

    fix: add docstring of github refresh_token response

--- apps/auth/src/app/services/GithubServices.ts
+++ apps/auth/src/app/services/GithubServices.ts
@@ -33,6 +33,14 @@ export class GithubService {
     }
 
     static async refreshAccess(refreshToken: string) {
+        // {
+        //     "access_token": "ghu_16C7e42F292c6912E7710c838347Ae178B4a",
+        //     "expires_in": "28800",
+        //     "refresh_token": "ghr_1B4a2e77838347a7E420ce178F2E7c6912E169246c34E1ccbF66C46812d16D5B1A9Dc86A1498",
+        //     "refresh_token_expires_in": "15811200",
+        //     "scope": "",
+        //     "token_type": "bearer"
+        //   }
         const r = await githubClient({
             url: 'https://github.com/login/oauth/access_token',
             method: 'POST',
