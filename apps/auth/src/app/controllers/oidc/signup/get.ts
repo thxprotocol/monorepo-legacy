2113d8fed5cf8058bb42b503411d456a912c3e8b
Garfdev <garfdev.13@gmail.com>
2023-01-05 16:14:47 +0700

    feat: add sso button for signup page

--- apps/auth/src/app/controllers/oidc/signup/get.ts
+++ apps/auth/src/app/controllers/oidc/signup/get.ts
@@ -1,7 +1,17 @@
+import { DiscordService } from '@thxnetwork/auth/services/DiscordService';
+import GithubService from '@thxnetwork/auth/services/GithubServices';
+import { TwitchService } from '@thxnetwork/auth/services/TwitchService';
+import { YouTubeService } from '@thxnetwork/auth/services/YouTubeService';
 import { Request, Response } from 'express';
 
 async function controller(req: Request, res: Response) {
     const { uid, params } = req.interaction;
+
+    params.googleLoginUrl = YouTubeService.getLoginUrl(req.params.uid, YouTubeService.getBasicScopes());
+    params.githubLoginUrl = GithubService.getLoginURL(uid, {});
+    params.discordLoginUrl = DiscordService.getLoginURL(uid, {});
+    params.twitchLoginUrl = TwitchService.getLoginURL(uid, {});
+
     res.render('signup', { uid, params });
 }
 
