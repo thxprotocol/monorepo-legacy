1228ae0f47e8632e717b5ee699292559dddba9f7
Peter <peter@thx.network>
2023-01-11 23:21:43 +0100

    Remove nodejs mixpanel

--- apps/auth/src/app/util/mixpanel.ts
+++
@@ -1,7 +0,0 @@
-import Mixpanel from 'mixpanel';
-import { MIXPANEL_TOKEN } from '../config/secrets';
-
-// create an instance of the mixpanel client
-export const mixpanel = Mixpanel.init(MIXPANEL_TOKEN, {
-    host: 'api-eu.mixpanel.com',
-});
