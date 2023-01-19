85bce7daa1fad4ae575617fa152255af2a4e144f
Garfdev <garfdev.13@gmail.com>
2023-01-19 11:55:58 +0700

    merge: develop

--- apps/dashboard/src/views/pool/Clients.vue
+++ apps/dashboard/src/views/pool/Clients.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="pb-5">
         <b-row class="mb-3">
             <b-col class="d-flex justify-content-end">
@@ -92,134 +91,6 @@
             />
         </BCard>
     </div>
-=======
-  <div>
-    <b-row class="mb-3">
-      <b-col class="d-flex align-items-center">
-        <h2 class="mb-0">Clients</h2>
-      </b-col>
-      <b-col class="d-flex justify-content-end">
-        <b-button v-b-modal="'modalRewardPointsCreate'" class="rounded-pill" variant="primary">
-          <i class="fas fa-plus mr-2"></i>
-          <span class="d-none d-md-inline">Clients</span>
-        </b-button>
-        <base-modal-client-create @hidden="onClose" :client="editingClient" :pool="pool" :page="page"
-          @submit="onSubmit" /> </b-col>
-    </b-row>
-    <BCard variant="white" body-class="p-0 shadow-sm">
-      <BaseCardTableHeader :page="page" :limit="limit" :pool="pool" :total-rows="totals[pool._id]"
-        :selectedItems="selectedItems" :actions="[{ variant: 0, label: `Delete rewards` }]"
-        @click-action="onClickAction" @change-page="onChangePage" @change-limit="onChangeLimit" />
-      <BTable :fields="fields" hover :busy="isLoading" :items="clientsByPage" responsive="lg" show-empty>
-        <!-- Head formatting -->
-        <template #head(checkbox)>
-          <b-form-checkbox @change="onChecked" />
-        </template>
-        <template #head(name)> Client Name </template>
-        <template #head(type)> Grant Type </template>
-        <template #head(clientInfo)> Client ID </template>
-        <template #head(id)> &nbsp; </template>
-
-        <!-- Cell formatting -->
-        <template #cell(checkbox)="{ item }">
-          <b-form-checkbox :value="item.checkbox" v-model="selectedItems" />
-        </template>
-        <template #cell(name)="{ item }">
-          <strong class="text-primary">{{ item.name }} </strong>
-        </template>
-        <template #cell(type)="{ item }">
-          {{ item.grantType }}
-        </template>
-        <template #cell(clientInfo)="{ item }">
-          <b-form-row>
-            <b-col md="3">
-              <label class="text-gray">Client ID</label>
-            </b-col>
-            <b-col>
-              <b-input-group size="sm" class="mb-2">
-                <b-form-input readonly size="sm" :value="item.clientId" />
-                <template #append>
-                  <b-button size="sm" variant="dark" v-clipboard:copy="item.clientId">
-                    <i class="fas fa-clipboard m-0"></i>
-                  </b-button>
-                </template>
-              </b-input-group>
-            </b-col>
-          </b-form-row>
-          <b-form-row>
-            <b-col md="3">
-              <label class="text-gray">Client Secret</label>
-            </b-col>
-            <b-col>
-              <b-input-group size="sm">
-                <b-form-input readonly size="sm" :value="item.clientSecret" />
-                <template #append>
-                  <b-button size="sm" variant="dark"
-                    v-clipboard:copy="item.clientSecret && Array.from({ length: item.clientSecret.length }).map(() => '•').join('')">
-                    <i class="fas fa-clipboard m-0"></i>
-                  </b-button>
-                </template>
-              </b-input-group>
-            </b-col>
-          </b-form-row>
-        </template>
-        <template #cell(id)="{ item }">
-          <b-dropdown variant="link" size="sm" no-caret>
-            <template #button-content>
-              <i class="fas fa-ellipsis-h ml-0 text-muted"></i>
-            </template>
-            <b-dropdown-item v-b-modal="'modalRewardPointsCreate' + item.id">Edit</b-dropdown-item>
-            <b-dropdown-item @click="$store.dispatch('clients/delete', clients[pool._id][item.id])">
-              Delete
-            </b-dropdown-item>
-          </b-dropdown>
-          <base-modal-client-create :id="'modalRewardPointsCreate' + item.id" @hidden="onClose"
-            :client="clients[pool._id][item.id]" :pool="pool" :page="page" @submit="onSubmit" />
-        </template>
-      </BTable>
-    </BCard>
-  </div>
-  <!-- <div>
-    <b-row class="mb-3">
-      <b-col class="d-flex align-items-center">
-        <h2 class="mb-0">Clients</h2>
-      </b-col>
-      <b-col class="d-flex justify-content-end">
-        <b-button
-          variant="primary"
-          v-b-modal="'modalClientCreate'"
-          class="rounded-pill"
-        >
-          <i class="fas fa-plus mr-2"></i>
-          Create Client
-        </b-button>
-        <base-modal-client-create
-          @hidden="onClose"
-          :client="editingClient"
-          :pool="pool"
-          :page="page"
-          @submit="onSubmit"
-        />
-      </b-col>
-    </b-row>
-    <base-list-item-client
-      @edit="onEdit"
-      :pool="pool"
-      :client="client"
-      :key="client.clientId"
-      v-for="client of clientsByPage"
-    />
-    <b-pagination
-      v-if="total > limit"
-      class="mt-3"
-      @change="onChangePage"
-      v-model="page"
-      :per-page="limit"
-      :total-rows="total"
-      align="center"
-    ></b-pagination>
-  </div> -->
->>>>>>> abedbe93 (fix: add watch deps back)
 </template>
 <script lang="ts">
 import { mapGetters } from 'vuex';
@@ -231,7 +102,6 @@ import { IPools } from '@thxnetwork/dashboard/store/modules/pools';
 import BaseCardTableHeader from '@thxnetwork/dashboard/components/cards/BaseCardTableHeader.vue';
 
 @Component({
-<<<<<<< HEAD
     components: {
         BaseListItemClient,
         BaseModalClientCreate,
@@ -247,59 +117,18 @@ export default class Clients extends Vue {
     page = 1;
     limit = 5;
     isLoading = true;
-=======
-  components: {
-    BaseListItemClient,
-    BaseModalClientCreate,
-    BaseCardTableHeader
-  },
-  computed: mapGetters({
-    totals: 'clients/totals',
-    clients: 'clients/all',
-    pools: 'pools/all',
-  }),
-})
-export default class Clients extends Vue {
-  [x: string]: any;
-  page = 1;
-  limit = 5;
-  isLoading = true;
-  selectedItems: string[] = [];
->>>>>>> abedbe93 (fix: add watch deps back)
 
     fields = ['checkbox', 'name', 'type', 'info', 'id'];
     actions = [];
 
-<<<<<<< HEAD
     shouldDisableActions = !this.actions.length;
-=======
-  fields = ['checkbox', 'name', 'type', 'clientInfo', 'id']
-
-
-  onChecked(checked: boolean) {
-    this.selectedItems = checked ? (this.clientsByPage.map((r) => r._id) as string[]) : [];
-  }
-
-  get total() {
-    return this.totals[this.$route.params.id];
-  }
->>>>>>> abedbe93 (fix: add watch deps back)
 
     totals!: { [poolId: string]: number };
     clients!: { [poolId: string]: { [id: string]: TClient } };
     editingClient: TClient | null = null;
     pools!: IPools;
 
-<<<<<<< HEAD
     selectedItems: string[] = [];
-=======
-  get clientsByPage() {
-    return Object.values(this.clients[this.$route.params.id])
-      .filter((client: TClient) => client.page === this.page)
-      .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1))
-      .slice(0, this.limit);
-  }
->>>>>>> abedbe93 (fix: add watch deps back)
 
     get total() {
         return this.totals[this.$route.params.id];
