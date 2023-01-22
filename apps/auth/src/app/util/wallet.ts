73ea5dfe7300b5d27a98f3b9b11b92d0ac91306a
Peter <peter@thx.network>
2023-01-22 19:42:22 +0100

    Remove wallet deploy for hardhat on signin

--- apps/auth/src/app/util/wallet.ts
+++ apps/auth/src/app/util/wallet.ts
@@ -2,13 +2,19 @@ import WalletProxy from '@thxnetwork/auth/proxies/WalletProxy';
 import { ChainId } from '@thxnetwork/auth/types/enums/chainId';
 import { AccountDocument } from '../models/Account';
 import { AccountVariant } from '../types/enums/AccountVariant';
+import { NODE_ENV } from '../config/secrets';
 
 export async function createWallet(account: AccountDocument) {
     if (account.variant === AccountVariant.Metamask) return;
 
     const sub = String(account._id);
+    const chains = [ChainId.Polygon];
 
-    for (const chainId of [ChainId.Polygon, ChainId.Hardhat]) {
+    if (NODE_ENV === 'development') {
+        chains.push(ChainId.Hardhat);
+    }
+
+    for (const chainId of chains) {
         const walletsCount = (await WalletProxy.get(sub, chainId)).length;
         if (!walletsCount) return WalletProxy.create(sub, chainId, false);
     }
