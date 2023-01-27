2071030bedd2c1fdf0905b1be4ae80018a179ceb
Peter <peter@thx.network>
2023-01-27 15:26:13 +0100

    Combine test cases

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -3,9 +3,6 @@ describe('Dashboard', () => {
         cy.visit('https://dev-dashboard.thx.network');
         cy.url().should('include', 'https://dev.auth.thx.network');
         cy.url().should('include', 'signin');
-    });
-
-    it('Redirect to signin/otp after submit', () => {
         cy.get('input[name="email"]').type('cypress@thx.network');
         cy.get('button[type="submit"]').click();
         cy.url().should('include', 'https://dev.auth.thx.network').should('include', 'signin/otp');
