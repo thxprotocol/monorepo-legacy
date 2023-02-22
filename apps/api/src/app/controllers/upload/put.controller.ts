aa3c44e8d483e802c9d75ca25f3505462c9853ef
Peter <peter@thx.network>
2023-02-22 22:12:40 +0100

    Implement IPFS uploads for premium accounts

--- apps/api/src/app/controllers/upload/put.controller.ts
+++ apps/api/src/app/controllers/upload/put.controller.ts
@@ -1,30 +1,23 @@
+import { Request, Response } from 'express';
+import { AccountPlanType } from '@thxnetwork/api/types/enums';
 import AccountProxy from '@thxnetwork/api/proxies/AccountProxy';
 import ImageService from '@thxnetwork/api/services/ImageService';
-import { AccountPlanType } from '@thxnetwork/api/types/enums';
-import { Request, Response } from 'express';
 import IPFSService from '@thxnetwork/api/util/ipfs';
 
 export default {
     controller: async (req: Request, res: Response) => {
-        let ipfsUrl;
         const file = req.file;
         if (!file) return res.status(440).send('There no file to process');
 
         const account = await AccountProxy.getById(req.auth.sub);
-
-        // if (req.body.ipfs && account.plan === AccountPlanType.Premium) {
-        if (account.plan === AccountPlanType.Premium) {
-            const result = await IPFSService.add(req.body.file);
-            console.log(result);
-
-            // const { data } = await IPFSService.get(result.data.Hash);
-            // console.log(data);
-
-            ipfsUrl = result.cid;
-        }
-
         const response = await ImageService.upload(file);
         const publicUrl = ImageService.getPublicUrl(response.key);
+
+        let ipfsUrl = 'https://ipfs.io/ipfs/';
+        if (account.plan === AccountPlanType.Premium) {
+            const result = await IPFSService.add(publicUrl);
+            ipfsUrl += result.cid.toString();
+        }
 
         res.send({ publicUrl, ipfsUrl });
     },
--- apps/api/src/app/util/ipfs.ts
+++ apps/api/src/app/util/ipfs.ts
@@ -1,27 +1,19 @@
 import { INFURA_IPFS_PROJECT_ID, INFURA_IPFS_PROJECT_SECRET } from '../config/secrets';
-import FormData from 'form-data';
+import { create, urlSource } from 'ipfs-http-client';
 
-const ipfsClient = async () => {
-    const { create } = await import('ipfs-http-client');
-    return await create({
-        host: 'ipfs.infura.io',
-        port: 5001,
-        protocol: 'https',
-        headers: {
-            Authorization: `Basic ${Buffer.from(`${INFURA_IPFS_PROJECT_ID}:${INFURA_IPFS_PROJECT_SECRET}`).toString(
-                'base64',
-            )}`,
-        },
-    });
-};
+const ipfsClient = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        Authorization: `Basic ${Buffer.from(`${INFURA_IPFS_PROJECT_ID}:${INFURA_IPFS_PROJECT_SECRET}`).toString(
+            'base64',
+        )}`,
+    },
+});
 
-export async function add(file) {
-    const client = await ipfsClient();
-    return client.add(file, { progress: () => console.log('progress') });
+export async function add(url: string) {
+    return await ipfsClient.add(urlSource(url));
 }
 
-export async function get(hash: string) {
-    return '';
-}
-
-export default { add, get };
+export default { add };
