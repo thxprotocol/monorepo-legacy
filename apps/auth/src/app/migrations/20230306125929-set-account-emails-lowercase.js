974611f4340aba515e6a959db2144683dc2f707a
Peter <peter@thx.network>
2023-03-07 10:20:04 +0100

    Only migrate accounts where email is set

--- apps/auth/src/app/migrations/20230306125929-set-account-emails-lowercase.js
+++ apps/auth/src/app/migrations/20230306125929-set-account-emails-lowercase.js
@@ -1,11 +1,10 @@
 module.exports = {
-  async up(db) {
-    await db.collection('accounts').updateMany(
-      {},
-      [{ $set: { email: { $toLower: "$email" } } }]
-    )
-  },
-  async down() {
-    //
-  },
+    async up(db) {
+        await db
+            .collection('accounts')
+            .updateMany({ email: { $exists: true } }, [{ $set: { email: { $toLower: '$email' } } }]);
+    },
+    async down() {
+        //
+    },
 };
