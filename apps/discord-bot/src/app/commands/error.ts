40c9d00a219fb24224fea5a3fc149e7f223aa9e6
Peter <peter@thx.network>
2023-03-15 12:10:06 +0100

    Expand error handler

--- apps/discord-bot/src/app/commands/error.ts
+++ apps/discord-bot/src/app/commands/error.ts
@@ -1,7 +1,6 @@
 import { CommandInteraction } from 'discord.js';
 
 export const onSubcommandError = async (interaction: CommandInteraction, error: Error) => {
-    console.log(error.message);
-
+    console.log(error.message, error);
     await interaction.reply({ content: 'An error occured.', ephemeral: true });
 };
