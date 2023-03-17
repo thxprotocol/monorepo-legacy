116e9e3c84703737572bc4ca9bbdeba9655858d9
Peter <peter@thx.network>
2023-03-17 08:28:11 +0100

    Fix price rule test

--- apps/auth/src/app/controllers/account/shopify/account.shopify.test.ts
+++ apps/auth/src/app/controllers/account/shopify/account.shopify.test.ts
@@ -11,7 +11,10 @@ import bcrypt from 'bcrypt';
 const http = request.agent(app);
 
 describe('Account Controller', () => {
-    let authHeader: string,
+    let basicAuthHeaderApi: string,
+        basicAuthHeaderDashboard: string,
+        authHeaderApi: string,
+        authHeaderDashboard: string,
         sub: string,
         uid = '',
         clientId = '',
@@ -24,19 +27,56 @@ describe('Account Controller', () => {
         beforeAll(async () => {
             await db.truncate();
 
-            const res = await http
-                .post('/reg')
-                .set({ Authorization: `Bearer ${INITIAL_ACCESS_TOKEN}` })
-                .send({
-                    application_type: 'web',
-                    client_name: 'THX Dashboard',
-                    grant_types: ['authorization_code'],
-                    redirect_uris: [redirectUri],
-                    response_types: ['code'],
-                    scope: 'openid pools:read pools:write withdrawals:read rewards:write deposits:read deposits:write wallets:read wallets:write',
-                });
+            async function requestToken(basicAuthHeader: string, payload: any) {
+                const res = await http
+                    .post('/token')
+                    .set({
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Authorization': basicAuthHeader,
+                    })
+                    .send(payload);
 
-            clientId = res.body.client_id;
+                return `Bearer ${res.body.access_token}`;
+            }
+
+            async function registerAPIClient() {
+                const res = await http
+                    .post('/reg')
+                    .set({ Authorization: `Bearer ${INITIAL_ACCESS_TOKEN}` })
+                    .send({
+                        application_type: 'web',
+                        client_name: 'THX API',
+                        grant_types: ['client_credentials'],
+                        redirect_uris: [],
+                        response_types: [],
+                        scope: 'openid accounts:read accounts:write',
+                    });
+
+                return 'Basic ' + Buffer.from(`${res.body.client_id}:${res.body.client_secret}`).toString('base64');
+            }
+
+            async function registerDashboardClient() {
+                const res = await http
+                    .post('/reg')
+                    .set({ Authorization: `Bearer ${INITIAL_ACCESS_TOKEN}` })
+                    .send({
+                        application_type: 'web',
+                        client_name: 'THX Dashboard',
+                        grant_types: ['authorization_code'],
+                        redirect_uris: [redirectUri],
+                        response_types: ['code'],
+                        scope: 'openid pools:read pools:write withdrawals:read rewards:write deposits:read deposits:write wallets:read wallets:write',
+                    });
+
+                clientId = res.body.client_id;
+            }
+
+            authHeaderApi = await requestToken(await registerAPIClient(), {
+                grant_type: 'client_credentials',
+                scope: 'openid accounts:read accounts:write',
+            });
+
+            await registerDashboardClient();
 
             mockWalletProxy();
         });
@@ -84,6 +124,8 @@ describe('Account Controller', () => {
                 // Override the hashed OTP in db to continue with a deterministic value
                 const hashedOtp = await bcrypt.hash(otp, 10);
                 const account = await AccountService.getByEmail(email);
+
+                sub = String(account._id);
 
                 account.setToken({ kind: AccessTokenKind.Auth, accessToken: hashedOtp });
                 await account.save();
@@ -225,7 +267,7 @@ describe('Account Controller', () => {
             it('GET /account/:sub/shopify/price-rules', (done) => {
                 http.get(`/account/${sub}/shopify/price-rules`)
                     .set({
-                        Authorization: authHeader,
+                        Authorization: authHeaderApi,
                     })
                     .expect(({ body }: request.Response) => {
                         console.log('body', body);
--- apps/auth/src/app/controllers/account/shopify/price-rules/list.action.ts
+++ apps/auth/src/app/controllers/account/shopify/price-rules/list.action.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
+import { param } from 'express-validator';
 import { AccountDocument } from '@thxnetwork/auth/models/Account';
 import { AccountService } from '@thxnetwork/auth/services/AccountService';
 import { ShopifyService } from '@thxnetwork/auth/services/ShopifyService';
 import { UnauthorizedError } from '@thxnetwork/auth/util/errors';
 import { AccessTokenKind } from '@thxnetwork/types/enums/AccessTokenKind';
+
+export const validation = [param('sub').isString()];
 
 export const getShopifyPriceRules = async (req: Request, res: Response) => {
     const account: AccountDocument = await AccountService.get(req.params.sub);
