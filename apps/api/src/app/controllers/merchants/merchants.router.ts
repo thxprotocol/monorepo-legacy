79f0761bc26a54d49b6f233102305991520a4a58
Peter <peter@thx.network>
2023-03-29 10:01:27 +0200

    Improve access check for merchants

--- apps/api/src/app/controllers/merchants/merchants.router.ts
+++ apps/api/src/app/controllers/merchants/merchants.router.ts
@@ -8,21 +8,33 @@ import DeleteMerchant from './delete.controller';
 
 const router = express.Router();
 
-router.post('/payments', guard.check('merchants:write'), CreateMerchantPayment.controller);
+router.post('/payments', CreateMerchantPayment.controller);
+
 router
     .use(checkJwt)
     .use(corsHandler)
     .post(
         '/link',
-        guard.check('merchants:write'),
+        guard.check(['merchants:write']),
         assertRequestInput(CreateMerchantLink.validation),
         CreateMerchantLink.controller,
     );
-router.use(checkJwt).use(corsHandler).get('/', guard.check('merchants:write'), ReadMerchant.controller);
-router.use(checkJwt).use(corsHandler).delete('/', guard.check('merchants:write'), DeleteMerchant.controller);
 router
     .use(checkJwt)
     .use(corsHandler)
-    .post('/', guard.check('merchant:write'), assertRequestInput(CreateMerchant.validation), CreateMerchant.controller);
+    .get('/', guard.check(['merchants:read']), ReadMerchant.controller);
+router
+    .use(checkJwt)
+    .use(corsHandler)
+    .delete('/', guard.check(['merchants:write']), DeleteMerchant.controller);
+router
+    .use(checkJwt)
+    .use(corsHandler)
+    .post(
+        '/',
+        guard.check(['merchant:write']),
+        assertRequestInput(CreateMerchant.validation),
+        CreateMerchant.controller,
+    );
 
 export default router;
