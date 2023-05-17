80f2303522dcf1072d9194977ed604dd035a628a
Peter <peter@thx.network>
2023-05-17 08:15:59 +0200

    Remove wei conversion for erc1155 balance

--- apps/api/src/app/controllers/erc1155/token/get.controller.ts
+++ apps/api/src/app/controllers/erc1155/token/get.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { param } from 'express-validator';
-import { fromWei } from 'web3-utils';
 import { NotFoundError } from '@thxnetwork/api/util/errors';
 import ERC1155Service from '@thxnetwork/api/services/ERC1155Service';
 
@@ -17,9 +16,7 @@ const controller = async (req: Request, res: Response) => {
     const metadata = await ERC1155Service.findMetadataById(token.metadataId);
     if (!metadata) throw new NotFoundError('ERC1155Metadata not found');
 
-    const balanceInWei = await erc1155.contract.methods.balanceOf(token.recipient, metadata.tokenId).call();
-    const balance = fromWei(balanceInWei, 'ether');
-
+    const balance = await erc1155.contract.methods.balanceOf(token.recipient, metadata.tokenId).call();
     const tokenUri = token.tokenId ? await erc1155.contract.methods.uri(token.tokenId).call() : '';
 
     res.status(200).json({
