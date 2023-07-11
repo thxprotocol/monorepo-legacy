acfda36947ccba3fc043798d86effdad54ed3855
Peter <peter@thx.network>
2023-07-11 14:19:56 +0200

    Open in new tab when auto nagivate on ios

--- apps/api/src/app/controllers/widget/js/get.controller.ts
+++ apps/api/src/app/controllers/widget/js/get.controller.ts
@@ -329,9 +329,6 @@ const controller = async (req: Request, res: Response) => {
                 : '<svg id="thx-svg-icon" style="display:block; margin: auto; fill: '+this.theme.elements.launcherIcon.color+'; width: 20px; height: 20px; transform: scale(1); transition: transform .2s ease;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M32 448c0 17.7 14.3 32 32 32h160V320H32v128zm256 32h160c17.7 0 32-14.3 32-32V320H288v160zm192-320h-42.1c6.2-12.1 10.1-25.5 10.1-40 0-48.5-39.5-88-88-88-41.6 0-68.5 21.3-103 68.3-34.5-47-61.4-68.3-103-68.3-48.5 0-88 39.5-88 88 0 14.5 3.8 27.9 10.1 40H32c-17.7 0-32 14.3-32 32v80c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-80c0-17.7-14.3-32-32-32zm-326.1 0c-22.1 0-40-17.9-40-40s17.9-40 40-40c19.9 0 34.6 3.3 86.1 80h-86.1zm206.1 0h-86.1c51.4-76.5 65.7-80 86.1-80 22.1 0 40 17.9 40 40s-17.9 40-40 40z"/></svg>';
             const launcher = document.createElement('div');
             launcher.id = 'thx-launcher';
-
-
-
             
             Object.assign(launcher.style, {
                 zIndex: 9999999,
@@ -349,6 +346,7 @@ const controller = async (req: Request, res: Response) => {
                 transform: 'scale(0)',
                 transition: '.2s opacity ease, .1s transform ease',
             });
+
             launcher.innerHTML = svgGift;
             launcher.addEventListener('click', this.onClickLauncher.bind(this));
             launcher.addEventListener('mouseenter', this.onMouseEnterLauncher.bind(this));
@@ -437,11 +435,17 @@ const controller = async (req: Request, res: Response) => {
             const redirectStatus = parentUrl.searchParams.get('redirect_status');
 
             if (widgetPath) {
-                const { widgetUrl, poolId, chainId, theme } = this.settings;
-                const path = '/' + poolId + widgetPath;
-                const url = new URL(widgetUrl + path);
-                
-                this.iframe.contentWindow.postMessage({ message: 'thx.iframe.navigate', path: url.pathname }, widgetUrl);
+                const isMobile = window.matchMedia('(pointer:coarse)').matches;
+
+                if (isMobile) {
+                    window.open(this.createURL(), '_blank');
+                } else {
+                    const { widgetUrl, poolId, chainId, theme } = this.settings;
+                    const path = '/' + poolId + widgetPath;
+                    const url = new URL(widgetUrl + path);
+
+                    this.iframe.contentWindow.postMessage({ message: 'thx.iframe.navigate', path: url.pathname }, widgetUrl);
+                }
             }
 
             if (this.ref) {
@@ -505,7 +509,7 @@ const controller = async (req: Request, res: Response) => {
         sourceMap: NODE_ENV !== 'production',
     });
 
-    res.set({ 'Content-Type': 'application/javascript' }).send(result.code);
+    res.set({ 'Content-Type': 'application/javascript' }).send(data);
 };
 
 export default { controller, validation };
