bfbc0dbda8f6eafca14c4751b3cb7bc49e24aa8c
Peter <peter@thx.network>
2023-07-26 14:34:17 +0200

    Adds env to init

--- scripts/safe-init.sh
+++ scripts/safe-init.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "==> $(date +%H:%M:%S) ==> Starting up environment containers..."
-docker compose -f docker-compose.safe.yml up --force-recreate -d
+docker compose --env-file .env.example -f docker-compose.safe.yml up --force-recreate -d
 sleep 60
 echo "==> $(date +%H:%M:%S) ==> Starting up environment containers..."
 docker compose exec txs-web python manage.py migrate
