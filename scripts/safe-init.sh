68de0f00183b8e1c7786b23bf21270dfdf741919
Peter <peter@thx.network>
2023-07-26 14:14:55 +0200

    Start safe-infra

--- scripts/safe-init.sh
+++ scripts/safe-init.sh
@@ -7,8 +7,8 @@ docker compose -f docker-compose.safe.yml up -d \
   && echo "==> $(date +%H:%M:%S) ==> Waiting for migrations... (may take a while)" \
   && sleep 60 \
   && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Config Service... (may take a while)" \
-  && docker compose exec cfg-web python src/manage.py createsuperuser \
+  && expect -c 'spawn docker compose exec cfg-web python src/manage.py createsuperuser; expect "Username:"; send "root\n"; expect "Email address:"; send "\n"; expect "Password:"; send "root\n"; expect "Password (again):"; send "root\n"; expect "Bypass password validation and create user anyway? [y/N]:"; send "yes\n"; interact' \
   && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Transaction Service... (may take a while)" \
-  && docker compose exec txs-web python manage.py createsuperuser || exit
+  && expect -c 'spawn docker compose exec txs-web python manage.py createsuperuser; expect "Username:"; send "root\n"; expect "Email address:"; send "\n"; expect "Password:"; send "root\n"; expect "Password (again):"; send "root\n"; expect "Bypass password validation and create user anyway? [y/N]:"; send "yes\n"; interact' || exit
 
 echo "==> $(date +%H:%M:%S) ==> All set! You may want to add a ChainInfo into the Config service. Please use the link below to fill its data: http://localhost:8000/cfg/admin/chains/chain/add/"
