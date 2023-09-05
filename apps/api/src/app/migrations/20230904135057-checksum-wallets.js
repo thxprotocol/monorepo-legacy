64d05590374a3c106f70cff2a9ed4cdfa16266d8
Peter <peter@thx.network>
2023-09-05 21:53:46 +0200

    Fix migration

--- apps/api/src/app/migrations/20230904135057-checksum-wallets.js
+++ apps/api/src/app/migrations/20230904135057-checksum-wallets.js
@@ -5,22 +5,37 @@ module.exports = {
         const walletsColl = await db.collection('wallets');
 
         for (const wallet of await walletsColl.find({}).toArray()) {
-            const address = toChecksumAddress(wallet.address);
-            await walletsColl.updateOne({ _id: wallet._id }, { $set: { address } });
+            try {
+                if (!wallet.address) continue;
+                const address = toChecksumAddress(wallet.address);
+                await walletsColl.updateOne({ _id: wallet._id }, { $set: { address } });
+            } catch (error) {
+                console.error(error);
+            }
         }
 
         const erc20Coll = await db.collection('erc20');
 
         for (const erc20 of await erc20Coll.find({}).toArray()) {
-            const address = toChecksumAddress(erc20.address);
-            await erc20Coll.updateOne({ _id: erc20._id }, { $set: { address } });
+            try {
+                if (!erc20.address) continue;
+                const address = toChecksumAddress(erc20.address);
+                await erc20Coll.updateOne({ _id: erc20._id }, { $set: { address } });
+            } catch (error) {
+                console.error(error);
+            }
         }
 
         const erc721Coll = await db.collection('erc721');
 
         for (const erc721 of await erc721Coll.find({}).toArray()) {
-            const address = toChecksumAddress(erc721.address);
-            await erc721Coll.updateOne({ _id: erc721._id }, { $set: { address } });
+            try {
+                if (!erc721.address) continue;
+                const address = toChecksumAddress(erc721.address);
+                await erc721Coll.updateOne({ _id: erc721._id }, { $set: { address } });
+            } catch (error) {
+                console.error(error);
+            }
         }
     },
 
