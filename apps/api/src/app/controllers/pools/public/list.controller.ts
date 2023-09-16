7160d9462e3cf5c2230717c0ac52ff3dd9215966
Peter <peter@thx.network>
2023-09-16 22:10:02 +0200

    Improve perf for campaign listing

--- apps/api/src/app/controllers/pools/public/list.controller.ts
+++ apps/api/src/app/controllers/pools/public/list.controller.ts
@@ -7,14 +7,8 @@ import { ChainId } from '@thxnetwork/types/enums';
 import BrandService from '@thxnetwork/api/services/BrandService';
 import PoolService from '@thxnetwork/api/services/PoolService';
 import { query } from 'express-validator';
-import { PaginationResult } from '@thxnetwork/api/util/pagination';
 
-export const paginatedResults = async (
-    model: any,
-    page: number,
-    limit: number,
-    search: string,
-): Promise<PaginationResult> => {
+export const paginatedResults = async (model: any, page: number, limit: number, search: string) => {
     const startIndex = (page - 1) * limit;
     const $match = {
         chainId: NODE_ENV === 'production' ? ChainId.Polygon : ChainId.Hardhat,
@@ -71,7 +65,58 @@ export const paginatedResults = async (
                 },
             },
             {
+                $lookup: {
+                    from: 'dailyrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'dailyquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'referralrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'invitequests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'pointrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'socialquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'milestonerewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'customquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'web3quests',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'web3quests',
+                },
+            },
+            {
                 $addFields: {
+                    totalQuestCount: {
+                        $size: {
+                            $concatArrays: [
+                                '$dailyquests',
+                                '$invitequests',
+                                '$socialquests',
+                                '$customquests',
+                                '$web3quests',
+                            ],
+                        },
+                    },
                     totalRewardsCount: {
                         $size: {
                             $concatArrays: ['$erc20Perks', '$erc721Perks', '$customRewards'],
@@ -104,6 +149,7 @@ export const paginatedResults = async (
                 $project: {
                     total: '$total.count',
                     results: 1,
+                    participantCount: 1,
                 },
             },
         ])
@@ -122,50 +168,53 @@ const controller = async (req: Request, res: Response) => {
     const result = await paginatedResults(AssetPool, Number(page), Number(limit), search ? String(search) : '');
 
     result.results = await Promise.all(
-        result.results.map(async (pool: AssetPoolDocument) => {
-            try {
-                const poolId = String(pool._id);
-                const widget = await Widget.findOne({ poolId });
-                if (!widget) return;
+        result.results.map(
+            async (
+                pool: AssetPoolDocument & {
+                    participantCount: number;
+                    totalQuestCount: number;
+                    totalRewardsCount: number;
+                },
+            ) => {
+                try {
+                    const poolId = String(pool._id);
+                    const widget = await Widget.findOne({ poolId });
+                    if (!widget) return;
 
-                const [brand, participants, quests, rewards] = await Promise.all([
-                    BrandService.get(poolId),
-                    PoolService.getParticipantCount(pool),
-                    PoolService.getQuestCount(pool),
-                    PoolService.getRewardCount(pool),
-                ]);
+                    const brand = await BrandService.get(poolId);
 
-                const progress = (() => {
-                    const data = {
-                        start: new Date(pool.createdAt).getTime(),
-                        now: Date.now(),
-                        end: new Date(pool.settings.endDate).getTime(),
+                    const progress = (() => {
+                        const data = {
+                            start: new Date(pool.createdAt).getTime(),
+                            now: Date.now(),
+                            end: new Date(pool.settings.endDate).getTime(),
+                        };
+                        const period = data.end - data.start;
+                        const progress = data.now - data.start;
+                        return (progress / period) * 100;
+                    })();
+
+                    return {
+                        _id: pool._id,
+                        title: pool.settings.title,
+                        expiryDate: pool.settings.endDate,
+                        address: pool.address,
+                        chainId: pool.chainId,
+                        domain: widget.domain,
+                        logoImgUrl: brand && brand.logoImgUrl,
+                        backgroundImgUrl: brand && brand.backgroundImgUrl,
+                        // tags: ['Gaming', 'Web3'],
+                        participants: pool.participantCount,
+                        rewards: pool.totalRewardsCount,
+                        quests: pool.totalQuestCount,
+                        active: widget.active,
+                        progress,
                     };
-                    const period = data.end - data.start;
-                    const progress = data.now - data.start;
-                    return (progress / period) * 100;
-                })();
-
-                return {
-                    _id: pool._id,
-                    title: pool.settings.title,
-                    expiryDate: pool.settings.endDate,
-                    address: pool.address,
-                    chainId: pool.chainId,
-                    domain: widget.domain,
-                    logoImgUrl: brand && brand.logoImgUrl,
-                    backgroundImgUrl: brand && brand.backgroundImgUrl,
-                    // tags: ['Gaming', 'Web3'],
-                    participants,
-                    rewards: rewards.length,
-                    quests: quests.length,
-                    active: widget.active,
-                    progress,
-                };
-            } catch (error) {
-                logger.error(error);
-            }
-        }),
+                } catch (error) {
+                    logger.error(error);
+                }
+            },
+        ),
     );
 
     res.json(result);
