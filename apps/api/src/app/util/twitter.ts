0f7607c5f8a12b7245d6bac2c5b1d6b2a199487c
Peter <peter@thx.network>
2023-10-05 11:47:58 +0200

    Cleanup

--- apps/api/src/app/util/twitter.ts
+++ apps/api/src/app/util/twitter.ts
@@ -1,8 +1,8 @@
 import { TWITTER_API_TOKEN } from '@thxnetwork/api/config/secrets';
 import axios, { AxiosRequestConfig } from 'axios';
 
-export function twitterClient(config: AxiosRequestConfig, accessToken?: string) {
-    axios.defaults.headers['Authorization'] = `Bearer ${accessToken || TWITTER_API_TOKEN}`;
+export function twitterClient(config: AxiosRequestConfig) {
+    axios.defaults.headers['Authorization'] = `Bearer ${TWITTER_API_TOKEN}`;
     axios.defaults.baseURL = 'https://api.twitter.com/2';
     return axios(config);
 }
