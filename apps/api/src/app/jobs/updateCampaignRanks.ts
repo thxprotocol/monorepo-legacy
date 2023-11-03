4d337231fac1513b90c72ed53893062747b09d05
Peter <peter@thx.network>
2023-11-03 15:01:57 +0100

    Improve campaign rank filter

--- apps/api/src/app/jobs/updateCampaignRanks.ts
+++ apps/api/src/app/jobs/updateCampaignRanks.ts
@@ -7,12 +7,6 @@ export async function updateCampaignRanks() {
     try {
         const campaigns = await AssetPool.aggregate([
             {
-                $match: {
-                    'settings.isPublished': true,
-                    'chainId': NODE_ENV === 'production' ? ChainId.Polygon : ChainId.Hardhat,
-                },
-            },
-            {
                 $addFields: {
                     id: { $toString: '$_id' },
                 },
@@ -26,27 +20,121 @@ export async function updateCampaignRanks() {
                 },
             },
             {
+                $lookup: {
+                    from: 'erc20perks',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'erc20Perks',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'erc721perks',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'erc721Perks',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'customrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'customRewards',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'couponrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'couponRewards',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'dailyrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'dailyquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'referralrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'invitequests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'pointrewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'socialquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'milestonerewards',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'customquests',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'web3quests',
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: 'web3quests',
+                },
+            },
+            {
                 $addFields: {
                     participantCount: { $size: '$participants' },
+                    totalQuestCount: {
+                        $size: {
+                            $concatArrays: [
+                                '$dailyquests',
+                                '$invitequests',
+                                '$socialquests',
+                                '$customquests',
+                                '$web3quests',
+                            ],
+                        },
+                    },
+                    totalRewardsCount: {
+                        $size: {
+                            $concatArrays: ['$erc20Perks', '$erc721Perks', '$customRewards', '$couponRewards'],
+                        },
+                    },
+                },
+            },
+            {
+                $match: {
+                    'settings.isPublished': true,
+                    'chainId': NODE_ENV === 'production' ? ChainId.Polygon : ChainId.Hardhat,
+                    'totalQuestCount': { $gt: 0 },
+                    'totalRewardsCount': { $gt: 0 },
                 },
             },
             {
                 $sort: { participantCount: -1 },
             },
         ]).exec();
-        console.log(campaigns);
 
-        const updates = campaigns.map((campaign, index) => {
-            return {
-                updateOne: {
-                    filter: { _id: campaign._id },
-                    update: { $set: { rank: Number(index) + 1 } },
-                },
-            };
-        });
-        console.log(updates);
-
-        await AssetPool.bulkWrite(updates);
+        await AssetPool.bulkWrite(
+            campaigns.map((campaign, index) => {
+                return {
+                    updateOne: {
+                        filter: { _id: campaign._id },
+                        update: { $set: { rank: Number(index) + 1 } },
+                    },
+                };
+            }),
+        );
     } catch (error) {
         logger.error(error);
     }
