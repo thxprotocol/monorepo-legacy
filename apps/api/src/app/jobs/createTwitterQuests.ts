d191f2fc7ee5dcc8c80d3205ef49ff5d3e981048
Peter <peter@thx.network>
2023-11-19 07:18:52 +0100

    Adds trycatch to twitter quest create

--- apps/api/src/app/jobs/createTwitterQuests.ts
+++ apps/api/src/app/jobs/createTwitterQuests.ts
@@ -14,67 +14,70 @@ function containsValue(text: string, hashtag: string) {
 }
 
 export async function createTwitterQuests() {
-    const endDate = new Date();
-    const startDate = subMinutes(endDate, 15);
-
     for await (const pool of AssetPool.find({ 'settings.isTwitterSyncEnabled': true })) {
-        const { isAuthorized } = await TwitterDataProxy.getTwitter(pool.sub);
-        if (!isAuthorized) continue;
+        const endDate = new Date();
+        const startDate = subMinutes(endDate, 15);
+        try {
+            const { isAuthorized } = await TwitterDataProxy.getTwitter(pool.sub);
+            if (!isAuthorized) continue;
 
-        const latestTweetsForPoolOwner = await TwitterDataProxy.getLatestTweets(pool.sub, startDate, endDate);
-        console.log({ startDate, endDate, latestTweetsForPoolOwner });
-        if (!latestTweetsForPoolOwner.length) continue;
+            const latestTweetsForPoolOwner = await TwitterDataProxy.getLatestTweets(pool.sub, startDate, endDate);
+            console.log({ startDate, endDate, latestTweetsForPoolOwner });
+            if (!latestTweetsForPoolOwner.length) continue;
 
-        const { hashtag, title, description, amount, isPublished } = pool.settings.defaults.conditionalRewards;
-        const latestTweets = await Promise.all(
-            latestTweetsForPoolOwner.map(async (tweet: any) => {
-                const isExistingQuest = await PointReward.exists({
-                    poolId: String(pool._id),
-                    content: tweet.id,
-                });
-                return { ...tweet, isExistingQuest };
-            }),
-        );
-        console.log({ latestTweets });
-        const filteredTweets = latestTweets.filter((tweet) => {
-            return !tweet.isExistingQuest && hashtag && containsValue(tweet.text, hashtag);
-        });
-        console.log({ filteredTweets });
-        if (!filteredTweets.length) continue;
-
-        const account: TAccount = await AccountProxy.getById(pool.sub);
-        const quests = await Promise.all(
-            filteredTweets.map(async (tweet) => {
-                try {
-                    const contentMetadata = JSON.stringify({
-                        url: `https://twitter.com/${account.twitterUsername}/status/${tweet.id}`,
-                        username: account.twitterUsername,
-                        text: tweet.text,
-                    });
-                    return await QuestService.create(QuestVariant.Twitter, pool._id, {
-                        index: 0,
-                        title,
-                        description,
-                        amount,
-                        platform: RewardConditionPlatform.Twitter,
-                        interaction: RewardConditionInteraction.TwitterLikeRetweet,
+            const { hashtag, title, description, amount, isPublished } = pool.settings.defaults.conditionalRewards;
+            const latestTweets = await Promise.all(
+                latestTweetsForPoolOwner.map(async (tweet: any) => {
+                    const isExistingQuest = await PointReward.exists({
+                        poolId: String(pool._id),
                         content: tweet.id,
-                        contentMetadata,
-                        isPublished,
                     });
-                } catch (error) {
-                    logger.error(error);
-                }
-            }),
-        );
-        console.log({ quests });
-        const subject = `Published ${quests.length} quest${quests.length && 's'}!`;
-        const message = `We have detected ${quests.length} new tweet${quests.length && 's'}. A Twitter Quest ${
-            quests.length && 'for each'
-        } has been ${isPublished ? 'published' : 'prepared'} for you.`;
+                    return { ...tweet, isExistingQuest };
+                }),
+            );
+            console.log({ latestTweets });
+            const filteredTweets = latestTweets.filter((tweet) => {
+                return !tweet.isExistingQuest && hashtag && containsValue(tweet.text, hashtag);
+            });
+            console.log({ filteredTweets });
+            if (!filteredTweets.length) continue;
 
-        await MailService.send(account.email, subject, message);
+            const account: TAccount = await AccountProxy.getById(pool.sub);
+            const quests = await Promise.all(
+                filteredTweets.map(async (tweet) => {
+                    try {
+                        const contentMetadata = JSON.stringify({
+                            url: `https://twitter.com/${account.twitterUsername}/status/${tweet.id}`,
+                            username: account.twitterUsername,
+                            text: tweet.text,
+                        });
+                        return await QuestService.create(QuestVariant.Twitter, pool._id, {
+                            index: 0,
+                            title,
+                            description,
+                            amount,
+                            platform: RewardConditionPlatform.Twitter,
+                            interaction: RewardConditionInteraction.TwitterLikeRetweet,
+                            content: tweet.id,
+                            contentMetadata,
+                            isPublished,
+                        });
+                    } catch (error) {
+                        logger.error(error);
+                    }
+                }),
+            );
+            console.log({ quests });
+            const subject = `Published ${quests.length} quest${quests.length && 's'}!`;
+            const message = `We have detected ${quests.length} new tweet${quests.length && 's'}. A Twitter Quest ${
+                quests.length && 'for each'
+            } has been ${isPublished ? 'published' : 'prepared'} for you.`;
 
-        logger.info(`[${pool.sub}] Created ${filteredTweets.length} Twitter Quests`);
+            await MailService.send(account.email, subject, message);
+
+            logger.info(`[${pool.sub}] Created ${filteredTweets.length} Twitter Quests`);
+        } catch (error) {
+            logger.info(error);
+        }
     }
 }
