3abff5f0a9efc8147404a3dfdbd638df3b305219
Peter <peter@thx.network>
2023-12-07 10:28:06 +0100

    Starts with topup flow

--- apps/api/src/app/controllers/pools/topup/post.controller.ts
+++ apps/api/src/app/controllers/pools/topup/post.controller.ts
@@ -1,52 +1,108 @@
 import { Request, Response } from 'express';
-import { BadRequestError, InsufficientBalanceError } from '@thxnetwork/api/util/errors';
-import { toWei } from 'web3-utils';
-import { MaxUint256, getProvider } from '@thxnetwork/api/util/network';
-import { ERC20Type } from '@thxnetwork/types/enums';
+import { InsufficientBalanceError, NotFoundError } from '@thxnetwork/api/util/errors';
 import { body, param } from 'express-validator';
-import ERC20Service from '@thxnetwork/api/services/ERC20Service';
-import TransactionService from '@thxnetwork/api/services/TransactionService';
+import { getAbiForContractName } from '@thxnetwork/api/config/contracts';
 import PoolService from '@thxnetwork/api/services/PoolService';
 import SafeService from '@thxnetwork/api/services/SafeService';
+import { MetaTransactionData } from '@safe-global/safe-core-sdk-types';
+import { ethers } from 'hardhat';
+import { BigNumber, ContractInterface } from 'ethers';
 
 export const validation = [param('id').isMongoId(), body('erc20Id').exists().isMongoId(), body('amount').exists()];
 
+const USDC_ADDRESS = '';
+const COMPANY_SAFE_ADDRESS = '';
+const BALANCER_VAULT_ADDRESS = '';
+const BALANCER_POOL_ID = '';
+
+enum JoinKind {
+    INIT,
+    EXACT_TOKENS_IN_FOR_BPT_OUT,
+    TOKEN_IN_FOR_EXACT_BPT_OUT,
+    ALL_TOKENS_IN_FOR_EXACT_BPT_OUT,
+}
+
 // TODO
-// 1. Create Split
-// 2. TransferFrom USDC to Splitter
-// 3. Receivers [campaignSafe, companySafe], [70, 30]
-// 4. Campaign Safe Deposit 100% in LP
-// 5. Transfer 75% to RewardDistributor
-// 6. Hold 25% for Quest Incentive Distribution (weekly recurring job with multisend)
+// 1. Customer transers 100% USDC to Campaign Safe
+// 2. Campaign Safe calls multiSend for 2 transactions
+// 2.1 transfer 30% of USDC to Company Safe
+// 2.2 joinPool 70% of USDC to LP
+// 2.3 transfer 75% of BPT to RewardDistributor
+// 3. hold 25% for Quest Incentive Distribution (weekly recurring job with multisend)
 
 const controller = async (req: Request, res: Response) => {
     // #swagger.tags = ['Pools']
     const pool = await PoolService.getById(req.header('X-PoolId'));
-    const { defaultAccount } = getProvider(pool.chainId);
-    const amount = toWei(String(req.body.amount));
-    const erc20 = await ERC20Service.getById(req.body.erc20Id);
-    if (erc20.type !== ERC20Type.Limited) throw new BadRequestError('Token type is not Limited type');
-
-    // Check balance to ensure throughput
-    const balance = await erc20.contract.methods.balanceOf(defaultAccount).call();
-    if (Number(balance) < Number(amount)) throw new InsufficientBalanceError();
+    if (!pool) throw new NotFoundError('Could not find campaign');
 
     // Check allowance for admin to ensure throughput
     const safe = await SafeService.findOneByPool(pool, pool.chainId);
-    const allowance = await erc20.contract.methods.allowance(defaultAccount, safe.address).call();
-    if (Number(allowance) < Number(amount)) {
-        await TransactionService.send(
-            erc20.contract.options.address,
-            erc20.contract.methods.approve(defaultAccount, MaxUint256),
-            pool.chainId,
-        );
-    }
 
-    await TransactionService.send(
-        erc20.contract.options.address,
-        erc20.contract.methods.transfer(safe.address, amount),
-        erc20.chainId,
+    // Check balance to ensure throughput
+    const usdc = new ethers.Contract(
+        USDC_ADDRESS,
+        getAbiForContractName('LimitedSupplyToken') as unknown as ContractInterface,
     );
+    const amount = BigNumber.from(req.body.amount);
+    const balance = await usdc.balanceOf(safe.address);
+    if (balance.lt(amount)) throw new InsufficientBalanceError('Insufficient USDC in campaign Safe for this topup');
+
+    const vault = new ethers.Contract(
+        BALANCER_VAULT_ADDRESS,
+        getAbiForContractName('BalancerVault') as unknown as ContractInterface,
+    );
+
+    // Prepare company safe USDC transfer
+    const tx1 = await usdc.populateTransaction.transfer(COMPANY_SAFE_ADDRESS, amount);
+
+    // joinPool(
+    //     bytes32 poolId,
+    //     address sender,
+    //     address recipient,
+    //     JoinPoolRequest request
+    // )
+
+    // struct JoinPoolRequest {
+    //     address[] assets,
+    //     uint256[] maxAmountsIn,
+    //     bytes userData,
+    //     bool fromInternalBalance
+    // }
+
+    const tx2 = await vault.populateTransaction.joinPool(BALANCER_POOL_ID, safe.address, safe.address, {
+        assets: [USDC_ADDRESS],
+        maxAmountsIn: [amount.mul(0.75)],
+        userData: JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT,
+        fromInternalBalance: false,
+    });
+
+    const tx3 = await vault.populateTransaction.joinPool(BALANCER_POOL_ID, safe.address, safe.address, {
+        assets: [USDC_ADDRESS],
+        maxAmountsIn: [amount.mul(0.25)],
+        userData: JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT,
+        fromInternalBalance: false,
+    });
+
+    // The batch to be sent. Order is important!
+    const payload: MetaTransactionData[] = [
+        {
+            to: USDC_ADDRESS,
+            value: '0',
+            data: tx1.data,
+        },
+        {
+            to: BALANCER_VAULT_ADDRESS,
+            value: '0',
+            data: tx2.data,
+        },
+        {
+            to: BALANCER_VAULT_ADDRESS,
+            value: '0',
+            data: tx3.data,
+        },
+    ];
+
+    // Execute the multisend
 
     res.status(200).end();
 };
