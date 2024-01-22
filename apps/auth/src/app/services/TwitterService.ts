45d3ef5f43f9bb906476c063be229a0875295fcd
Peter <peter@thx.network>
2024-01-22 23:57:27 +0100

    Log twitter query

--- apps/auth/src/app/services/TwitterService.ts
+++ apps/auth/src/app/services/TwitterService.ts
@@ -247,6 +247,7 @@ export class TwitterService {
     }
 
     static async searchTweets(token: IAccessToken, query: string) {
+        console.log(`from:${token.userId} ${query}`);
         const startTime = new Date(Date.now() - 60 * 60 * 24).toISOString(); // 24h ago
         const { data } = await twitterClient({
             url: '/tweets/search/recent',
