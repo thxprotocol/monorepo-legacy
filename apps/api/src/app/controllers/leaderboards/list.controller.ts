ce539eafc467f8048f9e513d19ef784e7e753bd5
Peter <peter@thx.network>
2024-01-24 16:15:35 +0100

    Improve campaign ranking

--- apps/api/src/app/controllers/leaderboards/list.controller.ts
+++ apps/api/src/app/controllers/leaderboards/list.controller.ts
@@ -1,192 +1,31 @@
 import { Request, Response } from 'express';
-import { NODE_ENV } from '@thxnetwork/api/config/secrets';
 import { AssetPool, AssetPoolDocument } from '@thxnetwork/api/models/AssetPool';
 import { Widget } from '@thxnetwork/api/models/Widget';
-import { logger } from '@thxnetwork/api/util/logger';
-import { ChainId } from '@thxnetwork/types/enums';
 import { query } from 'express-validator';
 import Brand from '@thxnetwork/api/models/Brand';
+import { Participant } from '@thxnetwork/api/models/Participant';
+
+const matchTitle = (search) => {
+    if (!search || !search.length) return;
+    return new RegExp(
+        search
+            .split(/\s+/)
+            .map((word) => `(?=.*${word})`)
+            .join(''),
+        'i',
+    );
+};
 
 export const paginatedResults = async (page: number, limit: number, search: string) => {
     const startIndex = (page - 1) * limit;
     const $match = {
+        'rank': { $exists: true },
         'settings.isPublished': true,
-        'chainId': NODE_ENV === 'production' ? ChainId.Polygon : ChainId.Hardhat,
-        'totalQuestCount': { $gt: 0 },
-        'totalRewardsCount': { $gt: 0 },
+        ...(search && { 'settings.title': matchTitle(search) }),
     };
+    const results = await AssetPool.find($match).sort({ rank: 1 }).skip(startIndex).limit(limit);
 
-    if (search) {
-        const $regex = new RegExp(
-            search
-                .split(/\s+/)
-                .map((word) => `(?=.*${word})`)
-                .join(''),
-            'i',
-        );
-        $match['settings.title'] = { $regex };
-    }
-    const results = await AssetPool.aggregate([
-        {
-            $addFields: {
-                id: { $toString: '$_id' },
-            },
-        },
-        {
-            $lookup: {
-                from: 'participants',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'participants',
-            },
-        },
-        {
-            $lookup: {
-                from: 'erc20perks',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'erc20Perks',
-            },
-        },
-        {
-            $lookup: {
-                from: 'erc721perks',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'erc721Perks',
-            },
-        },
-        {
-            $lookup: {
-                from: 'customrewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'customRewards',
-            },
-        },
-        {
-            $lookup: {
-                from: 'couponrewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'couponRewards',
-            },
-        },
-        {
-            $lookup: {
-                from: 'discordrolerewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'discordRoleRewards',
-            },
-        },
-        {
-            $lookup: {
-                from: 'dailyrewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'dailyquests',
-            },
-        },
-        {
-            $lookup: {
-                from: 'referralrewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'invitequests',
-            },
-        },
-        {
-            $lookup: {
-                from: 'pointrewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'socialquests',
-            },
-        },
-        {
-            $lookup: {
-                from: 'milestonerewards',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'customquests',
-            },
-        },
-        {
-            $lookup: {
-                from: 'web3quests',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'web3quests',
-            },
-        },
-        {
-            $lookup: {
-                from: 'gitcoinquests',
-                localField: 'id',
-                foreignField: 'poolId',
-                as: 'gitcoinquests',
-            },
-        },
-        {
-            $addFields: {
-                totalQuestCount: {
-                    $size: {
-                        $concatArrays: [
-                            '$dailyquests',
-                            '$invitequests',
-                            '$socialquests',
-                            '$customquests',
-                            '$web3quests',
-                            '$gitcoinquests',
-                        ],
-                    },
-                },
-                totalRewardsCount: {
-                    $size: {
-                        $concatArrays: [
-                            '$erc20Perks',
-                            '$erc721Perks',
-                            '$customRewards',
-                            '$couponRewards',
-                            '$discordRoleRewards',
-                        ],
-                    },
-                },
-                participantCount: { $size: '$participants' },
-            },
-        },
-        {
-            $facet: {
-                total: [{ $match }, { $count: 'count' }],
-                results: [
-                    { $match },
-                    {
-                        $sort: { rank: -1 },
-                    },
-                    {
-                        $skip: startIndex,
-                    },
-                    {
-                        $limit: limit,
-                    },
-                ],
-            },
-        },
-        {
-            $unwind: '$total',
-        },
-        {
-            $project: {
-                total: '$total.count',
-                results: 1,
-                participantCount: 1,
-            },
-        },
-    ]).exec();
-
-    if (!results[0]) return { limit, results: [] };
-    return { limit, ...results[0] };
+    return { limit, results };
 };
 
 const validation = [query('page').isInt(), query('limit').isInt(), query('search').optional().isString()];
@@ -198,55 +37,28 @@ const controller = async (req: Request, res: Response) => {
     const widgets = await Widget.find({ poolId: result.results.map((p: AssetPoolDocument) => p._id) });
     const brands = await Brand.find({ poolId: result.results.map((p: AssetPoolDocument) => p._id) });
 
-    result.results = (
-        await Promise.all(
-            result.results.map(
-                async (
-                    pool: AssetPoolDocument & {
-                        participantCount: number;
-                        totalQuestCount: number;
-                        totalRewardsCount: number;
-                    },
-                ) => {
-                    try {
-                        const poolId = String(pool._id);
-                        const widget = widgets.find((w) => w.poolId === poolId);
-                        const brand = brands.find((b) => b.poolId === poolId);
-
-                        let progress = 0;
-                        if (pool.settings.endDate) {
-                            const start = new Date(pool.createdAt).getTime();
-                            const now = Date.now();
-                            const end = new Date(pool.settings.endDate).getTime();
-                            const period = end - start;
-                            progress = ((now - start) / period) * 100;
-                        }
-
-                        return {
-                            _id: poolId,
-                            rank: pool.rank,
-                            slug: pool.settings.slug || poolId,
-                            title: pool.settings.title,
-                            expiryDate: pool.settings.endDate,
-                            address: pool.safeAddress,
-                            chainId: pool.chainId,
-                            domain: widget && widget.domain,
-                            logoImgUrl: brand && brand.logoImgUrl,
-                            backgroundImgUrl: brand && brand.backgroundImgUrl,
-                            // tags: ['Gaming', 'Web3'],
-                            participants: pool.participantCount,
-                            rewards: pool.totalRewardsCount,
-                            quests: pool.totalQuestCount,
-                            active: widget && widget.active,
-                            progress,
-                        };
-                    } catch (error) {
-                        logger.error(error);
-                    }
-                },
-            ),
-        )
-    ).filter((pool) => !!pool);
+    result.results = (await Promise.all(
+        result.results.map(async (pool) => {
+            const widget = widgets.find((w) => w.poolId === String(pool._id));
+            const brand = brands.find((b) => b.poolId === String(pool._id));
+            const participantCount = await Participant.countDocuments({ poolId: pool._id });
+            return {
+                _id: pool._id,
+                rank: pool.rank,
+                slug: pool.settings.slug || pool._id,
+                title: pool.settings.title,
+                expiryDate: pool.settings.endDate,
+                address: pool.safeAddress,
+                chainId: pool.chainId,
+                domain: widget ? widget.domain : 'https://www.thx.network',
+                logoImgUrl: brand && brand.logoImgUrl,
+                backgroundImgUrl: brand && brand.backgroundImgUrl,
+                // tags: ['Gaming', 'Web3'],
+                participants: participantCount,
+                active: widget && widget.active,
+            };
+        }),
+    )) as any;
 
     res.json(result);
 };
