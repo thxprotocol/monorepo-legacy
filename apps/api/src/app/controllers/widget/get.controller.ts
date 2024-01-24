8dbe251f944ff89b6ea3e125ec1d9b804a0031cb
Peter <peter@thx.network>
2024-01-24 19:49:05 +0100

    Adds description to widget config

--- apps/api/src/app/controllers/widget/get.controller.ts
+++ apps/api/src/app/controllers/widget/get.controller.ts
@@ -8,17 +8,15 @@ import { param } from 'express-validator';
 const validation = [param('id').isMongoId()];
 
 const controller = async (req: Request, res: Response) => {
-    // #swagger.tags = ['Widget']
     const widget = await Widget.findOne({ poolId: req.params.id });
     const pool = await AssetPool.findById(req.params.id);
     const brand = await Brand.findOne({ poolId: req.params.id });
-    const expired = pool.settings.endDate ? pool.settings.endDate.getTime() <= Date.now() : false;
 
     res.json({
         title: pool.settings.title,
+        description: pool.settings.description,
         logoUrl: brand ? brand.logoImgUrl : AUTH_URL + '/img/logo-padding.png',
         backgroundUrl: brand ? brand.backgroundImgUrl : '',
-        expired,
         theme: widget.theme,
         domain: widget.domain,
         chainId: pool.chainId,
