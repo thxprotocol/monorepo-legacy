d0a04844812f730f6b47108c720afc0c9d11c641
Peter <peter@thx.network>
2024-02-06 14:54:26 +0100

    Fix quest platform migration

--- apps/api/src/app/migrations/20240205094637-quest-platform-migration.js
+++ apps/api/src/app/migrations/20240205094637-quest-platform-migration.js
@@ -1,10 +1,27 @@
+// export enum RewardConditionPlatform {
+//     None = 0,
+//     Google = 1,
+//     Twitter = 2,
+//     Spotify = 3,
+//     Github = 4,
+//     Discord = 5,
+//     Twitch = 6,
+//     Shopify = 7,
+// }
+
 module.exports = {
     async up(db, client) {
         const questColl = db.collection('pointrewards');
 
         // Bulk change twitter quests for platform
-        await questColl.update({ platform: 1 }, { kind: 'twitter' });
-        await questColl.update({ platform: 1 }, { kind: 'twitter' });
+        await questColl.deleteOne({ platform: 0 });
+        await questColl.update({ platform: 1 }, { $set: { kind: 'google' } });
+        await questColl.update({ platform: 2 }, { $set: { kind: 'twitter' } });
+        await questColl.deleteOne({ platform: 3 }); //Remove
+        await questColl.deleteOne({ platform: 4 }); //Remove
+        await questColl.update({ platform: 5 }, { $set: { kind: 'discord' } });
+        await questColl.deleteOne({ platform: 6 }); //Remove
+        await questColl.deleteOne({ platform: 7 }); //Remove
     },
 
     async down(db, client) {
