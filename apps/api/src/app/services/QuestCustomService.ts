ed2e409ca2dec352ad1e9309b14d4a536a1f0af7
Peter <peter@thx.network>
2024-02-09 09:45:51 +0100

    Small refactor custom quest service

--- apps/api/src/app/services/QuestCustomService.ts
+++ apps/api/src/app/services/QuestCustomService.ts
@@ -1,6 +1,12 @@
-import { TAccount, TMilestoneReward, TMilestoneRewardClaim, TValidationResult } from '@thxnetwork/types/index';
+import {
+    TAccount,
+    TIdentity,
+    TMilestoneReward,
+    TMilestoneRewardClaim,
+    TValidationResult,
+} from '@thxnetwork/types/index';
 import { MilestoneReward, MilestoneRewardDocument } from '../models/MilestoneReward';
-import { Identity } from '../models/Identity';
+import { Identity, IdentityDocument } from '../models/Identity';
 import { Event } from '../models/Event';
 import { MilestoneRewardClaim } from '../models/MilestoneRewardClaims';
 import { WalletDocument } from '../models/Wallet';
@@ -33,15 +39,6 @@ export default class QuestCustomService implements IQuestService {
         return quest.amount;
     }
 
-    async findEntries({ quest, wallet }: { quest: MilestoneRewardDocument; wallet: WalletDocument }) {
-        if (!wallet) return [];
-        return MilestoneRewardClaim.find({
-            questId: quest._id,
-            walletId: wallet._id,
-            isClaimed: true,
-        });
-    }
-
     async decorate({
         quest,
         account,
@@ -54,11 +51,10 @@ export default class QuestCustomService implements IQuestService {
         data: Partial<TMilestoneRewardClaim>;
     }) {
         const entries = await this.findEntries({ quest, wallet });
-        const identities = wallet ? await Identity.find({ poolId: quest.poolId, sub: wallet.sub }) : [];
-        const identityIds = identities.map(({ _id }) => String(_id));
-        const events = identityIds.length ? await Event.find({ name: quest.eventName, identityId: identityIds }) : [];
-        const pointsAvailable = (quest.limit - entries.length) * quest.amount;
+        const identities = await this.findIdentities({ quest, wallet });
+        const events = await this.findEvents({ quest, identities });
         const isAvailable = await this.isAvailable({ quest, wallet, account, data });
+        const pointsAvailable = quest.limit ? (quest.limit - entries.length) * quest.amount : quest.amount;
 
         return {
             ...quest,
@@ -69,14 +65,17 @@ export default class QuestCustomService implements IQuestService {
         };
     }
 
-    async getValidationResult(options: {
-        quest: TMilestoneReward;
+    async getValidationResult({
+        quest,
+        wallet,
+    }: {
+        quest: MilestoneRewardDocument;
         account: TAccount;
         wallet: WalletDocument;
         data: Partial<TMilestoneRewardClaim>;
     }): Promise<{ reason: string; result: boolean }> {
         // See if there are identities
-        const identities = await Identity.find({ poolId: options.quest.poolId, sub: options.wallet.sub });
+        const identities = await Identity.find({ poolId: quest.poolId, sub: wallet.sub });
         if (!identities.length) {
             return {
                 result: false,
@@ -84,23 +83,47 @@ export default class QuestCustomService implements IQuestService {
             };
         }
 
-        const entries = await MilestoneRewardClaim.find({
-            questId: options.quest._id,
-            walletId: options.wallet._id,
-            isClaimed: true,
-        });
-        if (options.quest.limit && entries.length >= options.quest.limit) {
+        // Find existing entries for this quest and check optional limit
+        const entries = await this.findEntries({ quest, wallet });
+        if (quest.limit && entries.length >= quest.limit) {
             return { result: false, reason: 'Quest entry limit has been reached' };
         }
 
-        const events = await Event.find({
-            identityId: { $in: identities.map(({ _id }) => String(_id)) },
-            poolId: options.quest.poolId,
-            name: options.quest.eventName,
-        });
+        // Find events for this quest and the identities connected to the account
+        const events = await this.findEvents({ quest, identities });
         if (entries.length >= events.length) {
             return { result: false, reason: 'Insufficient custom events found for this quest' };
         }
+
         if (entries.length < events.length) return { result: true, reason: '' };
+    }
+
+    private async findEntries({ quest, wallet }: { quest: MilestoneRewardDocument; wallet: WalletDocument }) {
+        if (!wallet) return [];
+        return await this.models.entry.find({
+            questId: quest._id,
+            walletId: wallet._id,
+            isClaimed: true,
+        });
+    }
+
+    private async findIdentities({ quest, wallet }: { quest: MilestoneRewardDocument; wallet: WalletDocument }) {
+        if (!wallet) return [];
+        return await Identity.find({ poolId: quest.poolId, sub: wallet.sub });
+    }
+
+    private async findEvents({
+        quest,
+        identities,
+    }: {
+        quest: MilestoneRewardDocument;
+        identities: IdentityDocument[];
+    }) {
+        if (!identities.length) return [];
+        return await Event.find({
+            identityId: { $in: identities.map(({ _id }) => String(_id)) },
+            poolId: quest.poolId,
+            name: quest.eventName,
+        }).limit(quest.limit || null);
     }
 }
