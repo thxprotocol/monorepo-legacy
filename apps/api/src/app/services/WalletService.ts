7886dd238806050095ee155c5b8d2c8c6eb775a3
Peter <peter@thx.network>
2024-02-25 08:33:57 +0100

    Remove wallet connect

--- apps/api/src/app/services/WalletService.ts
+++ apps/api/src/app/services/WalletService.ts
@@ -46,21 +46,13 @@ export default class WalletService {
             [WalletVariant.Safe]: WalletService.createSafe,
             [WalletVariant.WalletConnect]: WalletService.createWalletConnect,
         };
-        return map[variant]({ ...data, chainId });
+        return map[variant]({ ...(data as TWallet), chainId });
     }
 
     static async createSafe({ sub, address, chainId }) {
         const safeWallet = await SafeService.findOne({ sub });
         // An account can have max 1 Safe
         if (safeWallet) throw new Error('Already has a Safe.');
-
-        // This query allows to take ownership of wallets owned by other subs as long as
-        // the address can be verified.
-        await Wallet.findOneAndUpdate(
-            { variant: WalletVariant.Web3Auth, address, chainId },
-            { variant: WalletVariant.Web3Auth, sub, address, chainId },
-            { upsert: true },
-        );
 
         // Deploy a Safe with Web3Auth address and relayer as signers
         await SafeService.create({ sub, chainId, safeVersion }, address);
--- libs/common/src/lib/types/enums/Wallet.ts
+++ libs/common/src/lib/types/enums/Wallet.ts
@@ -1,5 +1,4 @@
 export enum WalletVariant {
     Safe = 'safe',
     WalletConnect = 'walletconnect',
-    Web3Auth = 'web3auth',
 }
