4b4243f72de1022a6df8d52677172f4c10041130
Peter <peter@thx.network>
2024-03-13 10:45:52 +0100

    Other timetravel method

--- apps/api/src/app/controllers/ve/ve.test.ts
+++ apps/api/src/app/controllers/ve/ve.test.ts
@@ -34,8 +34,8 @@ describe('VESytem', () => {
         safeWallet = await SafeService.findOne({ sub, poolId: { $exists: false }, safeVersion: { $exists: true } });
         expect(safeWallet.address).toBeDefined();
 
-        // Travel past first week else this throws "Reward distribution has not started yet"
-        await timeTravel(60 * 60 * 24 * 7);
+        // // Travel past first week else this throws "Reward distribution has not started yet"
+        // await timeTravel(60 * 60 * 24 * 7);
 
         testBAL = new ethers.Contract(contractNetworks[chainId].BAL, contractArtifacts['BAL'].abi, signer);
         testBPT = new ethers.Contract(contractNetworks[chainId].BPT, contractArtifacts['BPT'].abi, signer);
