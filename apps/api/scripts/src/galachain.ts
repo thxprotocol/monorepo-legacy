e1ddfd6a0057c97837a7472ca4a16b226343e91c
Peter <peter@thx.network>
2024-03-21 13:44:00 +0100

    Improve payment error logging

--- apps/api/scripts/src/galachain.ts
+++ apps/api/scripts/src/galachain.ts
@@ -3,61 +3,54 @@ import { ChainUser, RestApiClientConfig, gcclient } from '@gala-chain/client';
 import { customAPI, customTokenAPI, getAdminPrivateKey, NETWORK_ROOT } from './utils/gala';
 import { BigNumber } from 'bignumber.js';
 import axios from 'axios';
+import { MaxUint256 } from '@thxnetwork/api/util/network';
 
 const adminPrivateKey = getAdminPrivateKey();
 
-// // Client Config
-// const params: RestApiClientConfig = {
-//     orgMsp: 'CuratorOrg',
-//     userId: 'admin',
-//     userSecret: 'adminpw',
-//     apiUrl: 'http://localhost:8801',
-//     configPath: path.resolve(NETWORK_ROOT, 'api-config.json'),
-// };
+// Client Config
+const params: RestApiClientConfig = {
+    orgMsp: 'CuratorOrg',
+    userId: 'admin',
+    userSecret: 'adminpw',
+    apiUrl: 'http://localhost:8801',
+    configPath: path.resolve(NETWORK_ROOT, 'api-config.json'),
+};
 
-// // Contract and channel defaults
-// const contract = {
-//     channelName: 'product-channel',
-//     chaincodeName: 'basic-product',
-// };
+// Contract and channel defaults
+const contract = {
+    channelName: 'product-channel',
+    chaincodeName: 'basic-product',
+};
 
-// // Client for user management
-// const client = gcclient
-//     .forApiConfig(params)
-//     .forContract({ ...contract, contractName: 'PublicKeyContract' })
-//     .extendAPI(customAPI);
+// Client for user management
+const client = gcclient
+    .forApiConfig(params)
+    .forContract({ ...contract, contractName: 'PublicKeyContract' })
+    .extendAPI(customAPI);
 
-// // Client for token management
-// const tokenClient = gcclient
-//     .forApiConfig(params)
-//     .forContract({ ...contract, contractName: 'GalaChainToken' })
-//     .extendAPI(customTokenAPI);
+// Client for token management
+const tokenClient = gcclient
+    .forApiConfig(params)
+    .forContract({ ...contract, contractName: 'GalaChainToken' })
+    .extendAPI(customTokenAPI);
 
 export default async function main() {
-    const payload = {
-        method: 'GalaChainToken:TransferToken',
-        args: [''],
-    };
-    const response = await axios({
-        url: 'http://localhost:8801/invoke',
-        method: 'POST',
-        data: {
-            method: 'GalaChainToken:TransferToken',
-            args: [payload],
-        },
-    });
-
     // Get admin profile
-    // const adminProfile = await client.GetProfile(adminPrivateKey);
-    // console.log(adminProfile);
-    // // Create a new user
-    // const newUser = ChainUser.withRandomKeys();
-    // // Register the user
-    // await client.RegisterEthUser(newUser);
-    // // Get profile for the user
-    // const newUserProfile = await client.GetProfile(newUser.privateKey);
-    // console.log(newUserProfile);
-    // // Deploy a Coin
+    const adminProfile = await client.GetProfile(adminPrivateKey);
+    console.log(adminProfile);
+
+    // Create a token distributor
+    const tokenDistributor = ChainUser.withRandomKeys();
+    console.log(tokenDistributor.privateKey);
+
+    // Register token distributor
+    await client.RegisterEthUser(tokenDistributor);
+
+    // Get profile for token distributor
+    const tokenDistributorProfile = await client.GetProfile(tokenDistributor.privateKey);
+    console.log({ tokenDistributor });
+
+    // Deploy a Coin
     // const coin = await tokenClient.ERC20Create(
     //     {
     //         image: 'https://thx.network',
@@ -70,39 +63,26 @@ export default async function main() {
     //     adminPrivateKey,
     // );
     // console.log(coin);
-    // // Approve
-    // await tokenClient.ERC20Approve(
-    //     {
-    //         spender: newUser,
-    //         amount: 100,
-    //     },
-    //     adminPrivateKey,
-    // );
-    // // Mint
-    // await tokenClient.ERC20Mint(
-    //     {
-    //         to: newUser,
-    //         amount: 100,
-    //     },
-    //     newUser.privateKey,
-    // );
-    // // Get Balance
-    // const [balance] = await tokenClient.ERC20BalanceOf({ owner: newUser });
-    // console.log(new BigNumber(balance.quantity).toNumber());
-    // // Deploy an NFT
-    // const nft = await tokenClient.ERC721Create(
-    //     {
-    //         name: 'THX Network',
-    //         description: 'THX Network NFT',
-    //         symbol: 'THXNFT',
-    //         image: 'https://thx.network',
-    //         tokenClass: {
-    //             collection: 'Items',
-    //             category: 'Weapons',
-    //             type: 'Axe',
-    //         },
-    //     },
-    //     adminPrivateKey,
-    // );
-    // console.log(nft);
+
+    // Approve
+    await tokenClient.ERC20Approve(
+        {
+            spender: tokenDistributor,
+            amount: 100000000,
+        },
+        adminPrivateKey,
+    );
+
+    // Mint
+    await tokenClient.ERC20Mint(
+        {
+            to: tokenDistributor,
+            amount: 100000000,
+        },
+        tokenDistributor.privateKey,
+    );
+
+    // Get Balance
+    const [balance] = await tokenClient.ERC20BalanceOf({ owner: tokenDistributor });
+    console.log(new BigNumber(balance.quantity).toNumber());
 }
--- apps/api/src/app/services/GalachainService.ts
+++ apps/api/src/app/services/GalachainService.ts
@@ -2,9 +2,12 @@ import { TokenInstanceKey, createValidDTO, TransferTokenDto } from '@gala-chain/
 import { plainToInstance } from 'class-transformer';
 import { BigNumber } from 'bignumber.js';
 import axios from 'axios';
+import { NODE_ENV } from '../config/secrets';
+import { logger } from '../util/logger';
+import { BadRequestError } from '../util/errors';
 
-// const GALACHAIN_URL = 'https://gateway.stage.galachain.com/api/hackathonXX';
-const GALACHAIN_URL = 'http://localhost:8801/invoke';
+const GALACHAIN_URL =
+    NODE_ENV === 'production' ? 'https://gateway.stage.galachain.com/api/hackathonXX' : 'http://localhost:8801/invoke';
 
 export default class GalachainService {
     static async createTransferDto(options: { to: string; amount: string; token: TGalachainToken }) {
@@ -18,20 +21,30 @@ export default class GalachainService {
 
     static async invokeContract(options: { contract: TGalachainContract; dto: TransferTokenDto; privateKey: string }) {
         const url = new URL(GALACHAIN_URL);
-        url.pathname = `${options.contract.channelName}/${options.contract.chaincodeName}`;
-        console.log(url.toString(), {
-            method: 'GalaChainToken:TransferToken',
-            args: [JSON.stringify(options.dto.signed(options.privateKey))],
-        });
+        url.pathname = `${url.pathname}/${options.contract.channelName}/${options.contract.chaincodeName}`;
 
-        const res = await axios({
-            method: 'POST',
-            url: url.toString(),
-            data: {
-                method: 'GalaChainToken:TransferToken',
-                args: [JSON.stringify(options.dto.signed(options.privateKey))],
-            },
-        });
-        console.log(res);
+        // console.log(url.toString(), {
+        //     method: 'GalaChainToken:TransferToken',
+        //     args: [JSON.stringify(options.dto.signed(options.privateKey))],
+        // });
+
+        const signedDto = options.dto.signed(options.privateKey);
+        try {
+            const res = await axios({
+                method: 'POST',
+                url: url.toString(),
+                headers: {
+                    Authorization: `Bearer ${signedDto.signature}`,
+                },
+                data: {
+                    method: 'GalaChainToken:TransferToken',
+                    args: [JSON.stringify(signedDto)],
+                },
+            });
+            console.log(res);
+        } catch (error) {
+            logger.error(error.response.data);
+            throw new BadRequestError(error.response.data.message);
+        }
     }
 }
