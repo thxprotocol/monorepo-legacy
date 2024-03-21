169006b96421925b69a69990c0de5b71aa74cd73
Peter <peter@thx.network>
2024-03-21 17:15:20 +0100

    Fix test url

--- apps/api/src/app/services/GalachainService.ts
+++ apps/api/src/app/services/GalachainService.ts
@@ -1,13 +1,11 @@
 import { TokenInstanceKey, createValidDTO, TransferTokenDto } from '@gala-chain/api';
 import { plainToInstance } from 'class-transformer';
 import { BigNumber } from 'bignumber.js';
-import axios from 'axios';
-import { NODE_ENV } from '../config/secrets';
 import { logger } from '../util/logger';
 import { BadRequestError } from '../util/errors';
+import axios from 'axios';
 
-const isProd = NODE_ENV === 'production';
-const GALACHAIN_URL = isProd ? 'https://gateway.stage.galachain.com/api/hackathon20' : 'http://localhost:8801/invoke';
+const GALACHAIN_URL = 'https://gateway.stage.galachain.com/api/';
 
 export default class GalachainService {
     static async createTransferDto(options: { to: string; amount: string; token: TGalachainToken }) {
@@ -20,29 +18,16 @@ export default class GalachainService {
     }
 
     static async invokeContract(options: { contract: TGalachainContract; dto: TransferTokenDto; privateKey: string }) {
+        const signedDto = options.dto.signed(options.privateKey);
         const url = new URL(GALACHAIN_URL);
         url.pathname = `${url.pathname}/${options.contract.channelName}/${options.contract.chaincodeName}`;
 
-        // console.log(url.toString(), {
-        //     method: 'GalaChainToken:TransferToken',
-        //     args: [JSON.stringify(options.dto.signed(options.privateKey))],
-        // });
-
-        const signedDto = options.dto.signed(options.privateKey);
         try {
-            const payload = JSON.stringify(signedDto);
             const res = await axios({
                 method: 'POST',
                 url: url.toString(),
-                headers: {
-                    Authorization: `Bearer ${signedDto.signature}`,
-                },
-                data: isProd
-                    ? payload
-                    : {
-                          method: 'GalaChainToken:TransferToken',
-                          args: [payload],
-                      },
+                headers: {},
+                data: signedDto.serialize(),
             });
             console.log(res);
         } catch (error) {
