3f19c2a10e9d2a7dd0e728762163b5467255e4e7
Peter <peter@thx.network>
2024-03-25 10:22:59 +0100

    Fix campaign ranking

--- apps/api/src/app/jobs/updateCampaignRanks.ts
+++ apps/api/src/app/jobs/updateCampaignRanks.ts
@@ -1,10 +1,46 @@
 import { ChainId } from '@thxnetwork/common/enums';
-import { Pool } from '@thxnetwork/api/models';
+import {
+    Pool,
+    RewardCoin,
+    RewardNFT,
+    RewardCustom,
+    RewardCoupon,
+    RewardDiscordRole,
+    RewardGalachain,
+    QuestDaily,
+    QuestInvite,
+    QuestSocial,
+    QuestCustom,
+    QuestWeb3,
+    QuestGitcoin,
+} from '@thxnetwork/api/models';
 import { NODE_ENV } from '@thxnetwork/api/config/secrets';
 import { logger } from '../util/logger';
 
 export async function updateCampaignRanks() {
     try {
+        const questModels = [QuestDaily, QuestInvite, QuestSocial, QuestCustom, QuestWeb3, QuestGitcoin];
+        const rewardModels = [RewardCoin, RewardNFT, RewardCustom, RewardCoupon, RewardDiscordRole, RewardGalachain];
+        const questLookupStages = questModels.map((model) => {
+            return {
+                $lookup: {
+                    from: model.collection.name,
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: model.collection.name,
+                },
+            };
+        });
+        const rewardLookupStages = rewardModels.map((model) => {
+            return {
+                $lookup: {
+                    from: model.collection.name,
+                    localField: 'id',
+                    foreignField: 'poolId',
+                    as: model.collection.name,
+                },
+            };
+        });
         const campaigns = await Pool.aggregate([
             {
                 $addFields: {
@@ -20,119 +56,19 @@ export async function updateCampaignRanks() {
                 },
             },
             // Rewards
-            {
-                $lookup: {
-                    from: 'erc20perks',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'erc20Perks',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'erc721perks',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'erc721Perks',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'customrewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'customRewards',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'couponrewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'couponRewards',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'discordrolerewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'discordRoleRewards',
-                },
-            },
-            // Quests
-            {
-                $lookup: {
-                    from: 'dailyrewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'dailyquests',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'referralrewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'invitequests',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'pointrewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'socialquests',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'milestonerewards',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'customquests',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'web3quests',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'web3quests',
-                },
-            },
-            {
-                $lookup: {
-                    from: 'gitcoinquests',
-                    localField: 'id',
-                    foreignField: 'poolId',
-                    as: 'gitcoinquests',
-                },
-            },
+            ...questLookupStages,
+            ...rewardLookupStages,
             {
                 $addFields: {
                     participantCount: { $size: '$participants' },
                     totalQuestCount: {
                         $size: {
-                            $concatArrays: [
-                                '$dailyquests',
-                                '$invitequests',
-                                '$socialquests',
-                                '$customquests',
-                                '$web3quests',
-                                '$gitcoinquests',
-                            ],
+                            $concatArrays: questModels.map((model) => `$${model.collection.name}`),
                         },
                     },
                     totalRewardsCount: {
                         $size: {
-                            $concatArrays: [
-                                '$erc20Perks',
-                                '$erc721Perks',
-                                '$customRewards',
-                                '$couponRewards',
-                                '$discordRoleRewards',
-                            ],
+                            $concatArrays: rewardModels.map((model) => `$${model.collection.name}`),
                         },
                     },
                 },
