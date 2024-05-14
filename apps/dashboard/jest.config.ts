f355c3c0698b772820c8272c57561cb5ca8a26cb
Peter <peter@thx.network>
2024-05-14 11:25:11 +0200

    Upgrade NX 16

--- apps/dashboard/jest.config.ts
+++ apps/dashboard/jest.config.ts
@@ -1,21 +1,22 @@
 module.exports = {
-  displayName: 'dashboard',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+.vue$': '@vue/vue2-jest',
-    '.+.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
-  coverageDirectory: '../../coverage/apps/dashboard',
-  snapshotSerializers: ['jest-serializer-vue'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'apps/dashboard/tsconfig.spec.json',
+    displayName: 'dashboard',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^.+.vue$': '@vue/vue2-jest',
+        '.+.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'apps/dashboard/tsconfig.spec.json',
+            },
+        ],
     },
-    'vue-jest': {
-      tsConfig: 'apps/dashboard/tsconfig.spec.json',
+    moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+    coverageDirectory: '../../coverage/apps/dashboard',
+    snapshotSerializers: ['jest-serializer-vue'],
+    globals: {
+        'vue-jest': {
+            tsConfig: 'apps/dashboard/tsconfig.spec.json',
+        },
     },
-  },
 };
