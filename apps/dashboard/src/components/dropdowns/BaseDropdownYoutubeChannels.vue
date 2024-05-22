de8fc256ced2ff753c94e2da84f6ee68d91b917e
Peter <peter@thx.network>
2024-05-22 09:06:11 +0200

    Adds new privacy policy

--- apps/dashboard/src/components/dropdowns/BaseDropdownYoutubeChannels.vue
+++ apps/dashboard/src/components/dropdowns/BaseDropdownYoutubeChannels.vue
@@ -3,6 +3,14 @@
         <b-input-group prepend="#">
             <b-form-input @change="onChangeInput" :value="content" />
         </b-input-group>
+        <template #description>
+            <b-link
+                target="_blank"
+                href="https://support.google.com/youtube/answer/3250431?hl=en#:~:text=Find%20your%20channel's%20user%20ID%20%26%20channel%20ID&text=Sign%20in%20to%20YouTube.&text=From%20the%20left%20menu%2C%20select,channel's%20user%20and%20channel%20IDs."
+            >
+                Learn how to find your YouTube Channel ID.
+            </b-link>
+        </template>
     </b-form-group>
 </template>
 
